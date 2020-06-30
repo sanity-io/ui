20f3a3b02d867e655d82edeb20f0720e789f776a
Marius Lundgård <studio@mariuslundgard.com>
2020-06-30 13:41:04 +0200

    Update layout examples

--- docs/pages/layout.tsx
+++ docs/pages/layout.tsx
@@ -20,90 +20,144 @@ function LayoutPage() {
         <Box as="section">
           <h2>Box</h2>
 
-          <Box padding={4} style={{outline: '1px solid red'}}>
-            Test
-          </Box>
+          <Card padding={2} tone="transparent">
+            <Box padding={4} style={{outline: '1px solid red'}}>
+              <Text>Text</Text>
+            </Box>
+          </Card>
 
-          <Card as="pre" padding={2}>
-            <code>{`import {Box} from '@sanity/ui'
-
-<Box padding={4}>Test</Box>`}</code>
+          <Card as="pre" padding={2} tone="contrast">
+            <code>{`<Box padding={4} style={{outline: '1px solid red'}}>
+  <Text>Text</Text>
+</Box>`}</code>
           </Card>
         </Box>
 
         <Box as="section">
           <h2>Card</h2>
 
-          <Card padding={4}>Test</Card>
+          <Card padding={2} tone="transparent">
+            <Card padding={4}>
+              <Text>Text</Text>
+            </Card>
+          </Card>
 
-          <Card as="pre" padding={2}>
-            <code>{`import {Card} from '@sanity/ui'
-
-<Card padding={4}>Test</Card>`}</code>
+          <Card as="pre" padding={2} tone="contrast">
+            <code>{`<Card padding={4}>
+  <Text>Text</Text>
+</Card>`}</code>
           </Card>
         </Box>
 
         <Box as="section">
           <h2>Stack</h2>
 
-          <Stack space={4}>
-            <Text>foo</Text>
-            <Text>bar</Text>
-            <Text>baz</Text>
-          </Stack>
+          <Card padding={2} tone="transparent">
+            <Stack space={4}>
+              <Card>
+                <Text>foo</Text>
+              </Card>
+              <Card>
+                <Text>bar</Text>
+              </Card>
+              <Card>
+                <Text>baz</Text>
+              </Card>
+            </Stack>
+          </Card>
 
-          <Card as="pre" padding={2}>
-            <code>{`import {Stack} from '@sanity/ui'
-
-<Stack padding={4}>Test</Stack>`}</code>
+          <Card as="pre" padding={2} tone="contrast">
+            <code>{`<Stack space={4}>
+  <Card>
+    <Text>foo</Text>
+  </Card>
+  <Card>
+    <Text>bar</Text>
+  </Card>
+  <Card>
+    <Text>baz</Text>
+  </Card>
+</Stack>`}</code>
           </Card>
         </Box>
 
         <Box as="section">
           <h2>Inline</h2>
 
-          <Inline space={4}>
-            <Card padding={1}>1</Card>
-            <Card padding={2}>2</Card>
-            <Card padding={3}>3</Card>
-          </Inline>
+          <Card padding={2} tone="transparent">
+            <Inline space={4} style={{textAlign: 'center'}}>
+              <Card padding={1}>
+                <Text>foo</Text>
+              </Card>
+              <Card padding={2}>
+                <Text>bar</Text>
+              </Card>
+              <Card padding={3}>
+                <Text>baz</Text>
+              </Card>
+            </Inline>
+          </Card>
 
-          <Card as="pre" padding={2}>
-            <code>{`import {Inline} from '@sanity/ui'
-
-<Inline padding={4}>Test</Inline>`}</code>
+          <Card as="pre" padding={2} tone="contrast">
+            <code>{`<Inline space={4} style={{textAlign: 'center'}}>
+  <Card padding={1}>
+    <Text>foo</Text>
+  </Card>
+  <Card padding={2}>
+    <Text>bar</Text>
+  </Card>
+  <Card padding={3}>
+    <Text>baz</Text>
+  </Card>
+</Inline>`}</code>
           </Card>
         </Box>
 
         <Box as="section">
           <h2>Flex</h2>
 
-          <Flex>
-            <Card flex={1}>1</Card>
-            <Card flex={[1, 2, 3]}>2</Card>
-          </Flex>
+          <Card padding={2} tone="transparent">
+            <Flex>
+              <Card flex={1}>hello</Card>
+              <Card flex={[1, 2, 3]}>world</Card>
+            </Flex>
+          </Card>
 
-          <Flex direction="column">
-            <Card flex={1}>1</Card>
-            <Card flex={[1]}>2</Card>
-          </Flex>
+          <Card as="pre" padding={2} tone="contrast">
+            <code>{`<Flex>
+  <Card flex={1}>hello</Card>
+  <Card flex={[1, 2, 3]}>world</Card>
+</Flex>`}</code>
+          </Card>
 
-          <Card as="pre" padding={2}>
-            <code>{`import {Flex} from '@sanity/ui'
+          <Card padding={2} tone="transparent">
+            <Flex direction="column">
+              <Card flex={1}>hello</Card>
+              <Card flex={[1]}>world</Card>
+            </Flex>
+          </Card>
 
-<Flex padding={4}>Test</Flex>`}</code>
+          <Card as="pre" padding={2} tone="contrast">
+            <code>{`<Flex direction="column">
+  <Card flex={1}>hello</Card>
+  <Card flex={[1]}>world</Card>
+</Flex>`}</code>
           </Card>
         </Box>
 
         <Box as="section">
           <h2>Container</h2>
 
-          <Container width={4}>Test</Container>
+          <Card padding={2} tone="transparent">
+            <Container width={0}>
+              <Text>Contained text</Text>
+            </Container>
+          </Card>
 
-          <Card as="pre" padding={2}>
-            <code>{`import {Container} from '@sanity/ui'
-
-<Container padding={4}>Test</Container>`}</code>
+          <Card as="pre" padding={2} tone="contrast">
+            <code>{`<Container width={0}>
+  <Text>Contained text</Text>
+</Container>`}</code>
           </Card>
         </Box>
       </Box>
