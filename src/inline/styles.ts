742b81e4d90cbbcde27e33d9c737b29fdf8448f1
Marius Lundgård <studio@mariuslundgard.com>
2020-07-01 14:20:42 +0200

    Update Inline component

--- src/inline/styles.ts
+++ src/inline/styles.ts
@@ -1,6 +1,10 @@
 import {css} from 'styled-components'
 import {Theme} from '../theme'
 
+function rem(px: number) {
+  return `${px / 16}rem`
+}
+
 export function inlineBaseStyles() {
   return css`
     display: block;
@@ -13,27 +17,25 @@ export function inlineBaseStyles() {
 }
 
 export function inlineSpaceStyles(props: {space: number[]; theme: Theme}) {
-  const {space} = props.theme
-
   return css`
     ${props.space.map((spaceIndex, mqIndex) => {
-      const ems = space[spaceIndex] / 16
+      const space = rem(props.theme.space[spaceIndex])
 
       if (mqIndex === 0)
         return css`
-          margin: -${ems}rem 0 0 -${ems}rem;
+          margin: -${space} 0 0 -${space};
 
           & > div {
-            padding: ${ems}rem 0 0 ${ems}rem;
+            padding: ${space} 0 0 ${space};
           }
         `
 
       return css`
-        @media (min-width: ${props.theme.media[mqIndex - 1]}px) {
-          margin: -${ems}rem 0 0 -${ems}rem;
+        @media (min-width: ${props.theme.media[mqIndex - 1] / 16}rem) {
+          margin: -${space} 0 0 -${space};
 
           & > div {
-            padding: ${ems}rem 0 0 ${ems}rem;
+            padding: ${space} 0 0 ${space};
           }
         }
       `
