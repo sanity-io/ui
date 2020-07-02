a7a8c5f655331dd4b1a7bbf70ec3b2f6a42a6471
Marius Lundgård <studio@mariuslundgard.com>
2020-07-02 16:05:43 +0200

    Simplify Next config

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,20 +1,17 @@
 'use strict'
 
 const path = require('path')
-const resolve = require('resolve')
 
 const ROOT_PATH = path.dirname(__dirname)
 
 module.exports = {
   webpack: (config) => {
     // Includes
-    config.module.rules[0].include.push(path.resolve(__dirname, '../src'))
+    config.module.rules[0].include.push(path.join(ROOT_PATH, 'src'))
 
     // Aliases
     Object.assign(config.resolve.alias, {
-      '@sanity/ui': path.resolve(ROOT_PATH, 'src'),
-      react: resolve.sync('react', {basedir: ROOT_PATH}),
-      'react-dom': resolve.sync('react-dom', {basedir: ROOT_PATH}),
+      '@sanity/ui': path.join(ROOT_PATH, 'src'),
     })
 
     return config
