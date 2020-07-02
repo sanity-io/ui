8c7c0f987b72ab8308252135c486e3a416e88c10
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2020-07-02 12:24:50 +0200

    Label component

---
+++ src/label/label.stories.tsx
@@ -0,0 +1,26 @@
+import {withCentered} from '~/storybook/decorators'
+import {Label} from '@sanity/ui'
+import {select, withKnobs} from '@storybook/addon-knobs'
+import React from 'react'
+
+export default {
+  title: 'Label',
+  decorators: [withCentered, withKnobs],
+}
+
+export const plain = () => {
+  const size = select(
+    'Size',
+    {'0': 0, '1': 1, '2 (default)': undefined, '3': 3, '4': 4},
+    undefined,
+    'Props'
+  )
+
+  return (
+    <div style={{background: '#fff'}}>
+      <Label size={size}>Hello, world</Label>
+      <Label size={size}>Hello, world</Label>
+      <Label size={size}>Hello, world</Label>
+    </div>
+  )
+}
