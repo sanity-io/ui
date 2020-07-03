f7911bcbc6ed5141f6191b20b333d72fd327622f
Marius Lundgård <studio@mariuslundgard.com>
2020-07-03 14:52:22 +0200

    Pass on props needed by styled-components overrides

--- src/radio/radio.tsx
+++ src/radio/radio.tsx
@@ -11,9 +11,11 @@ const Input = styled.input(inputElementStyles)
 const Representation = styled.div(representationStyles)
 
 export const Radio = forwardRef((props: React.HTMLProps<HTMLInputElement> & RadioProps, ref) => {
+  const {className, style, ...restProps} = props
+
   return (
-    <Root data-ui="Radio">
-      <Input type="radio" {...props} ref={ref} />
+    <Root className={className} data-ui="Radio" style={style}>
+      <Input type="radio" {...restProps} ref={ref} />
       <Representation data-name="representation" />
     </Root>
   )
