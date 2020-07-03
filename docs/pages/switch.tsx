dfd69a05e466dabc4368f6514f4dc26876716a6f
Marius Lundgård <studio@mariuslundgard.com>
2020-07-03 14:41:03 +0200

    Update Switch guide

--- docs/pages/switch.tsx
+++ docs/pages/switch.tsx
@@ -1,26 +1,41 @@
 import {AppLayout, CodeBlock} from '~/components'
-import {Card, Stack, Switch} from '@sanity/ui'
+import {Card, Heading, Inline, Stack, Switch} from '@sanity/ui'
 import Head from 'next/head'
 import React, {useState} from 'react'
-function SwitchPage() {
-  const [checked, setChecked] = useState(false)
+
+function SwitchExample(props: {checked?: boolean}) {
+  const [checked, setChecked] = useState(props.checked)
+
   const handleChange = () => {
     setChecked(!checked)
   }
+
+  return <Switch checked={checked} onChange={handleChange} style={{verticalAlign: 'top'}} />
+}
+
+function SwitchPage() {
   return (
     <>
       <Head>
         <title>Switch – Sanity UI</title>
       </Head>
+
       <AppLayout>
-        <h1>Switch</h1>
         <Stack space={4}>
-          <Card padding={3} radius={2} tone="transparent">
-            <Switch checked={checked} onChange={handleChange} />
+          <Heading>Switch</Heading>
+
+          <Card padding={3} radius={2} tone="transparent" style={{textAlign: 'center'}}>
+            <Inline space={2}>
+              <SwitchExample checked />
+              <SwitchExample checked={false} />
+              <SwitchExample />
+            </Inline>
           </Card>
-          <CodeBlock>{`<Card padding={3} radius={2} tone="transparent">
-  <Switch checked={checked} onChange={handleChange} />
-</Card>`}</CodeBlock>
+          <CodeBlock>{`<Inline space={2}>
+  <Switch checked />
+  <Switch checked={false} />
+  <Switch />
+</Inline>`}</CodeBlock>
         </Stack>
       </AppLayout>
     </>
