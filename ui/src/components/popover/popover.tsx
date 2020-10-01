1c2d77f986f8eb392899b0a7912bd3e9d88a2fd8
Marius Lundgård <studio@mariuslundgard.com>
2020-10-01 13:18:03 +0200

    Wrap popover in forwardRef

--- ui/src/components/popover/popover.tsx
+++ ui/src/components/popover/popover.tsx
@@ -1,4 +1,4 @@
-import React, {cloneElement, useEffect, useState} from 'react'
+import React, {cloneElement, forwardRef, useEffect, useState} from 'react'
 import {usePopper} from 'react-popper'
 import {Card} from '../card'
 import {PopoverArrow} from './arrow'
@@ -27,93 +27,101 @@ interface PopoverProps {
   referenceElement?: HTMLElement | null
 }
 
-export function Popover(
-  props: PopoverProps & Omit<React.HTMLProps<HTMLDivElement>, 'children' | 'content'>
-) {
-  const {
-    boundaryElement,
-    children: child,
-    content,
-    disabled,
-    open,
-    padding,
-    placement = 'bottom',
-    radius = 2,
-    referenceElement: referenceElementProp,
-    style = {},
-    ...restProps
-  } = props
-  const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(null)
-  const [popperElement, setPopperElement] = useState<HTMLElement | null>(null)
-  const [arrowElement, setArrowElement] = useState<HTMLElement | null>(null)
-  const popperReferenceElement = referenceElementProp || referenceElement
-  const popper = usePopper(popperReferenceElement, popperElement, {
-    placement,
-    modifiers: [
-      {
-        name: 'arrow',
-        options: {
-          element: arrowElement,
-          padding: 4,
+export const Popover = forwardRef(
+  (props: PopoverProps & Omit<React.HTMLProps<HTMLDivElement>, 'children' | 'content'>, ref) => {
+    const {
+      boundaryElement,
+      children: child,
+      content,
+      disabled,
+      open,
+      padding,
+      placement = 'bottom',
+      radius = 2,
+      referenceElement: referenceElementProp,
+      style = {},
+      ...restProps
+    } = props
+    const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(null)
+    const [popperElement, setPopperElement] = useState<HTMLElement | null>(null)
+    const [arrowElement, setArrowElement] = useState<HTMLElement | null>(null)
+    const popperReferenceElement = referenceElementProp || referenceElement
+    const popper = usePopper(popperReferenceElement, popperElement, {
+      placement,
+      modifiers: [
+        {
+          name: 'arrow',
+          options: {
+            element: arrowElement,
+            padding: 4,
+          },
         },
-      },
-      {
-        name: 'preventOverflow',
-        options: {
-          altAxis: true,
-          boundary: boundaryElement || undefined,
-          padding: 8,
+        {
+          name: 'preventOverflow',
+          options: {
+            altAxis: true,
+            boundary: boundaryElement || undefined,
+            padding: 8,
+          },
         },
-      },
-      {
-        name: 'offset',
-        options: {
-          offset: [0, 4],
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 4],
+          },
         },
-      },
-    ],
-  })
+      ],
+    })
 
-  const {attributes, forceUpdate, styles} = popper
+    const {attributes, forceUpdate, styles} = popper
 
-  useEffect(() => {
-    if (forceUpdate) forceUpdate()
-  }, [forceUpdate, content, popperReferenceElement])
+    useEffect(() => {
+      if (forceUpdate) forceUpdate()
+    }, [forceUpdate, content, popperReferenceElement])
 
-  if (disabled) {
-    return child || <></>
-  }
-
-  const setRef = (el: HTMLElement | null) => {
-    const childRef = (child as any).ref
-
-    setReferenceElement(el)
-
-    if (typeof childRef === 'function') {
-      childRef(el)
-    } else if (childRef) {
-      childRef.current = el
+    if (disabled) {
+      return child || <></>
     }
+
+    const setRef = (el: HTMLElement | null) => {
+      const childRef = (child as any).ref
+
+      setReferenceElement(el)
+
+      if (typeof childRef === 'function') {
+        childRef(el)
+      } else if (childRef) {
+        childRef.current = el
+      }
+    }
+
+    const setRootRef = (el: HTMLDivElement | null) => {
+      setPopperElement(el)
+      if (typeof ref === 'function') ref(el)
+      else if (ref) ref.current = el
+    }
+
+    return (
+      <>
+        {child && !referenceElementProp ? cloneElement(child, {ref: setRef}) : child || <></>}
+
+        {open && (
+          <div
+            {...restProps}
+            ref={setRootRef}
+            style={{...style, ...styles.popper}}
+            {...attributes.popper}
+          >
+            <Card padding={padding} radius={radius} shadow={3}>
+              {content}
+            </Card>
+
+            <PopoverArrow ref={setArrowElement} tone="default" style={styles.arrow} />
+          </div>
+        )}
+      </>
+    )
   }
+)
 
-  return (
-    <>
-      {child && !referenceElementProp ? cloneElement(child, {ref: setRef}) : child || <></>}
-
-      {open && (
-        <div
-          {...restProps}
-          ref={setPopperElement}
-          style={{...style, ...styles.popper}}
-          {...attributes.popper}
-        >
-          <Card padding={padding} radius={radius} shadow={3}>
-            {content}
-          </Card>
-
-          <PopoverArrow ref={setArrowElement} tone="default" style={styles.arrow} />
-        </div>
-      )}
-    </>
-  )
-}
+Popover.displayName = 'Popover'
