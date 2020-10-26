4b07fec9bf7bbef955455a927ed2db252b92c9d9
Marius Lundgård <studio@mariuslundgard.com>
2020-10-26 16:00:04 +0100

    Fix Next.js build (by downgrading to React v16)

--- ui-docs/next.config.js
+++ ui-docs/next.config.js
@@ -6,14 +6,25 @@ const ROOT_PATH = path.dirname(__dirname, '..')
 
 module.exports = {
   webpack: (config) => {
-    // Includes
+    // Add monorepo sibling packages to includes
     config.module.rules[0].include.push(path.join(ROOT_PATH, 'icons/src'))
     config.module.rules[0].include.push(path.join(ROOT_PATH, 'ui/src'))
+
+    // Add `react` to externals
+    // config.externals = (config.externals || []).concat('react')
 
     // Aliases
     Object.assign(config.resolve.alias, {
       '@sanity/icons': path.join(ROOT_PATH, 'icons/src'),
       '@sanity/ui': path.join(ROOT_PATH, 'ui/src'),
+
+      // React v17
+      // 'react/jsx-runtime': require.resolve('react/jsx-runtime'),
+      // 'react/jsx-dev-runtime': require.resolve('react/jsx-dev-runtime'),
+
+      react: require.resolve('react'),
+      'react-dom': require.resolve('react-dom'),
+      'styled-components': require.resolve('styled-components'),
     })
 
     return config
