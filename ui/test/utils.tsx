d7f515c8dd58d6fb9ea87dd7d710daa9b0660d92
Marius Lundgård <studio@mariuslundgard.com>
2020-10-27 21:44:10 +0100

    Add axe test for Badge

--- ui/test/utils.tsx
+++ ui/test/utils.tsx
@@ -1,22 +1,31 @@
 import {render as tlRender, RenderOptions as TLRenderOptions} from '@testing-library/react'
 import React from 'react'
-import {studioTheme, ThemeProvider} from '../src/theme'
+import {CardProvider} from '../src/atoms/card'
+import {ColorSchemeKey, studioTheme, ThemeProvider} from '../src/theme'
 
 interface RenderOptions extends TLRenderOptions {
+  scheme?: ColorSchemeKey
   strict?: boolean
 }
 
 const DefaultWrapper: React.FC = ({children}: any) => <main>{children}</main>
 
 export function render(element: React.ReactElement<any>, options: RenderOptions = {}) {
-  const {baseElement, strict = false, wrapper: InnerWrapper = DefaultWrapper} = options
+  const {
+    baseElement,
+    scheme = 'light',
+    strict = false,
+    wrapper: InnerWrapper = DefaultWrapper,
+  } = options
   const Mode = strict ? React.StrictMode : React.Fragment
 
   const Wrapper: React.FC = ({children}: any) => {
     return (
       <Mode>
         <InnerWrapper>
-          <ThemeProvider theme={studioTheme}>{children}</ThemeProvider>
+          <ThemeProvider theme={studioTheme}>
+            <CardProvider scheme={scheme}>{children}</CardProvider>
+          </ThemeProvider>
         </InnerWrapper>
       </Mode>
     )
