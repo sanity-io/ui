e4499e18973412746b53815ac718483c164b12d3
Marius Lundgård <studio@mariuslundgard.com>
2020-10-27 20:44:55 +0100

    Wrap Inline in `forwardRef`

--- ui/src/atoms/inline/inline.tsx
+++ ui/src/atoms/inline/inline.tsx
@@ -10,18 +10,20 @@ interface InlineProps {
 
 const Root = styled.div(inlineBaseStyles, inlineSpaceStyles)
 
-export const Inline = forwardRef((props: React.HTMLProps<HTMLDivElement> & InlineProps, ref) => {
-  const {children: childrenProp, space: spaceProp, ...restProps} = props
-  const children = childrenToElementArray(childrenProp).filter(Boolean)
-  const space = getResponsiveProp(spaceProp, [])
+export const Inline = forwardRef(
+  (props: InlineProps & Omit<React.HTMLProps<HTMLDivElement>, 'space'>, ref) => {
+    const {children: childrenProp, space: spaceProp, ...restProps} = props
+    const children = childrenToElementArray(childrenProp).filter(Boolean)
+    const space = getResponsiveProp(spaceProp, [])
 
-  return (
-    <Root data-ui="Inline" {...restProps} ref={ref} space={space}>
-      {children.map((child, idx) => (
-        <div key={idx}>{child}</div>
-      ))}
-    </Root>
-  )
-})
+    return (
+      <Root data-ui="Inline" {...restProps} ref={ref} space={space}>
+        {children.map((child, idx) => (
+          <div key={idx}>{child}</div>
+        ))}
+      </Root>
+    )
+  }
+)
 
 Inline.displayName = 'Inline'
