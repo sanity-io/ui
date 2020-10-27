e79bede6cef61259cd7e1cfd0d92d1c26e89ac55
Marius Lundgård <studio@mariuslundgard.com>
2020-10-27 13:29:11 +0100

    Fix build config

--- ui-docs/next.config.js
+++ ui-docs/next.config.js
@@ -4,34 +4,30 @@ const path = require('path')
 
 const ROOT_PATH = path.dirname(__dirname, '..')
 
-const __DEV__ = process.env.NODE_ENV === 'development'
-
 module.exports = {
   webpack: (config) => {
-    if (__DEV__) {
-      // Add monorepo sibling packages to includes
-      config.module.rules[0].include.push(path.join(ROOT_PATH, 'color/src'))
-      config.module.rules[0].include.push(path.join(ROOT_PATH, 'icons/src'))
-      config.module.rules[0].include.push(path.join(ROOT_PATH, 'ui/src'))
+    // Add monorepo sibling packages to includes
+    config.module.rules[0].include.push(path.join(ROOT_PATH, 'color/src'))
+    config.module.rules[0].include.push(path.join(ROOT_PATH, 'icons/src'))
+    config.module.rules[0].include.push(path.join(ROOT_PATH, 'ui/src'))
 
-      // Add `react` to externals
-      // config.externals = (config.externals || []).concat('react')
+    // Add `react` to externals
+    // config.externals = (config.externals || []).concat('react')
 
-      // Aliases
-      Object.assign(config.resolve.alias, {
-        '@sanity/color': path.join(ROOT_PATH, 'color/src'),
-        '@sanity/icons': path.join(ROOT_PATH, 'icons/src'),
-        '@sanity/ui': path.join(ROOT_PATH, 'ui/src'),
+    // Aliases
+    Object.assign(config.resolve.alias, {
+      '@sanity/color': path.join(ROOT_PATH, 'color/src'),
+      '@sanity/icons': path.join(ROOT_PATH, 'icons/src'),
+      '@sanity/ui': path.join(ROOT_PATH, 'ui/src'),
 
-        // React v17
-        // 'react/jsx-runtime': require.resolve('react/jsx-runtime'),
-        // 'react/jsx-dev-runtime': require.resolve('react/jsx-dev-runtime'),
+      // React v17
+      // 'react/jsx-runtime': require.resolve('react/jsx-runtime'),
+      // 'react/jsx-dev-runtime': require.resolve('react/jsx-dev-runtime'),
 
-        react: require.resolve('react'),
-        'react-dom': require.resolve('react-dom'),
-        'styled-components': require.resolve('styled-components'),
-      })
-    }
+      react: require.resolve('react'),
+      'react-dom': require.resolve('react-dom'),
+      'styled-components': require.resolve('styled-components'),
+    })
 
     return config
   },
