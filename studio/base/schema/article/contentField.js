2165f3c85e2c1f985a4a54f0384a1fe3931bc7e8
Marius Lundgård <studio@mariuslundgard.com>
2020-10-27 16:15:52 +0100

    Add code to contentField

--- studio/base/schema/article/contentField.js
+++ studio/base/schema/article/contentField.js
@@ -1,9 +1,14 @@
 import {codeExampleField} from './codeExampleField'
 import {propertyTableField} from './propertyTableField'
+const codeField = {
+  type: 'code',
+  name: 'code',
+  title: 'Code',
+}
 
 export const contentField = {
   type: 'array',
   name: 'content',
   title: 'Content',
-  of: [{type: 'block'}, codeExampleField, propertyTableField],
+  of: [{type: 'block'}, codeField, codeExampleField, propertyTableField],
 }
