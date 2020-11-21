9a38f111f5bbb63fab7f142d59038518beeed225
Marius Lundgård <studio@mariuslundgard.com>
2020-11-21 10:15:57 +0100

    [chore] Improve e2e tests for MenuButton

--- .qawolf/menuButton.test.ts
+++ .qawolf/menuButton.test.ts
@@ -15,14 +15,58 @@ afterAll(async () => {
   await browser.close()
 })
 
-test('components/MenuButton', async () => {
-  const page = await context.newPage()
+describe('components/MenuButton', () => {
+  it('should open menu', async () => {
+    const page = await context.newPage()
 
-  await page.goto(
-    'http://localhost:9009/iframe.html?id=components-menu--menu-button&viewMode=story',
-    {waitUntil: 'domcontentloaded'}
-  )
+    await page.goto(
+      'http://localhost:9009/iframe.html?id=components-menu--menu-button&viewMode=story',
+      {waitUntil: 'domcontentloaded'}
+    )
 
-  await page.click('button[data-ui="Avatar"]')
-  await page.waitForSelector('[data-ui="MenuItem"]')
+    await page.click('button[data-ui="Avatar"]')
+    await page.waitForSelector('[data-ui="MenuItem"]')
+  })
+
+  it('should use arrow keys to navigate the menu', async () => {
+    const page = await context.newPage()
+
+    await page.goto(
+      'http://localhost:9009/iframe.html?id=components-menu--menu-button&viewMode=story',
+      {waitUntil: 'domcontentloaded'}
+    )
+
+    // Open menu by pressed DOWN arrow key
+    await page.press('#menu-button', 'ArrowDown')
+    await page.waitForSelector('#menu-item-1:focus')
+
+    // Move through menu with arrow keys
+    await page.press('#menu-item-1', 'ArrowDown')
+    await page.waitForSelector('#menu-item-2:focus')
+    await page.press('#menu-item-2', 'ArrowDown')
+    // Skips #menu-item-3, because it's disabled
+    await page.waitForSelector('#menu-item-4:focus')
+    await page.press('#menu-item-4', 'ArrowDown')
+    // The first menu item should now be focused
+    await page.waitForSelector('#menu-item-1:focus')
+    // Escape to exit the menu
+    await page.press('#menu-item-1', 'Escape')
+    await page.waitForSelector('#menu-button:focus')
+
+    // Open menu by pressed UP arrow key
+    await page.press('#menu-button', 'ArrowUp')
+    await page.waitForSelector('#menu-item-4:focus')
+    // Move through menu with arrow keys
+    await page.press('#menu-item-4', 'ArrowUp')
+    // Skips #menu-item-3, because it's disabled
+    await page.waitForSelector('#menu-item-2:focus')
+    await page.press('#menu-item-2', 'ArrowUp')
+    await page.waitForSelector('#menu-item-1:focus')
+    await page.press('#menu-item-1', 'ArrowUp')
+    // The last menu item should now be focused
+    await page.waitForSelector('#menu-item-4:focus')
+    // Escape to exit the menu
+    await page.press('#menu-item-4', 'Escape')
+    await page.waitForSelector('#menu-button:focus')
+  })
 })
