ad2ae923ae5c90832bbfc40ee2f66e1d96d4db09
Marius Lundgård <studio@mariuslundgard.com>
2020-11-21 10:14:45 +0100

    [ui] Improve MenuButton stories

--- ui/src/components/menu/menu.stories.tsx
+++ ui/src/components/menu/menu.stories.tsx
@@ -18,20 +18,53 @@ export default {
   title: 'Components/Menu',
 }
 
+const preventMouseEvent = (event: React.MouseEvent) => event.preventDefault()
+
 export const menu = () => (
   <LayerProvider>
     <Card>
       <Menu>
-        <MenuItem as="a" href="#" icon="add" text="Foo" />
-        <MenuItem icon="add" text="Bar" />
+        <MenuItem
+          as="a"
+          href="#"
+          icon="add"
+          id="menu-item-1"
+          onClick={preventMouseEvent}
+          text="Foo"
+        />
+        <MenuItem icon="add" id="menu-item-2" text="Bar" />
         <MenuDivider />
-        <MenuItem icon="add" iconRight="checkmark" text="Baz" />
+        <MenuItem icon="add" iconRight="checkmark" id="menu-item-1" text="Baz" />
       </Menu>
     </Card>
   </LayerProvider>
 )
 
 export const menuButton = () => (
+  <LayerProvider>
+    <MenuButton
+      button={<Avatar as="button" color="magenta" size={2} />}
+      id="menu-button"
+      menu={
+        <Menu>
+          <MenuItem icon="search" id="menu-item-1" onClick={action('Search')} text="Search" />
+          <MenuItem icon="clock" id="menu-item-2" onClick={action('Clock')} text="Clock" />
+          <MenuItem
+            disabled
+            icon="comment"
+            id="menu-item-3"
+            onClick={action('Comment')}
+            text="Comment"
+          />
+          <MenuDivider />
+          <MenuItem icon="expand" id="menu-item-4" onClick={action('Expand')} text="Expand" />
+        </Menu>
+      }
+    />
+  </LayerProvider>
+)
+
+export const menuButtonWithGroup = () => (
   <LayerProvider>
     <MenuButton
       button={<Avatar as="button" color="magenta" size={2} />}
