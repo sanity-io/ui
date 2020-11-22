f01b37fe76c151ab4918cbb3e24f22dcdb148bde
Marius Lundgård <studio@mariuslundgard.com>
2020-11-22 10:38:29 +0100

    [ui] Improve Autocomplete

--- ui/src/components/autocomplete/autocomplete.tsx
+++ ui/src/components/autocomplete/autocomplete.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react'
+import React, {cloneElement, useCallback, useEffect, useRef, useState} from 'react'
 import styled from 'styled-components'
 import {Box, Button, Card, Text, TextInput} from '../../atoms'
 import {focusFirstDescendant} from '../../helpers'
@@ -17,7 +17,7 @@ export interface AutocompleteProps {
   options: AutocompleteOption[]
   padding?: number | number[]
   radius?: number | number[]
-  renderOption?: (option: AutocompleteOption) => React.ReactNode
+  renderOption?: (option: AutocompleteOption) => React.ReactElement
   size?: number | number[]
   value?: string
 }
@@ -58,6 +58,20 @@ const defaultRenderOption = ({value}: {value: string}) => (
   </Card>
 )
 
+const LIST_IGNORE_KEYS = [
+  'Control',
+  'Shift',
+  'Alt',
+  'Enter',
+  'Home',
+  'End',
+  'PageUp',
+  'PageDown',
+  'Meta',
+  'Tab',
+  'CapsLock',
+]
+
 export function Autocomplete(props: Props) {
   const {
     border = true,
@@ -77,18 +91,79 @@ export function Autocomplete(props: Props) {
   const [value, setValue] = useState(valueProp)
   const valueRef = useRef(value)
   const [focused, setFocused] = useState(false)
-  const [listHovered, setListHovered] = useState(false)
   const inputId = `${id}-input`
   const listboxId = `${id}-listbox`
   const options = Array.isArray(optionsProp) ? optionsProp : []
   const optionsLen = options.length
   const expanded = focused && optionsLen > 0
   const [selectedIndex, setSelectedIndex] = useState(-1)
-  const listRef = useRef<HTMLUListElement | null>(null)
   const inputRef = useRef<HTMLInputElement | null>(null)
+  const listRef = useRef<HTMLUListElement | null>(null)
   const activeItemId = selectedIndex ? `${id}-option-${selectedIndex}` : undefined
   // const activeValue = (selectedIndex && options[selectedIndex]?.value) || null
   const padding = getResponsiveProp(paddingProp)
+  const rootRef = useRef<HTMLDivElement | null>(null)
+
+  const handleRootBlur = useCallback(() => {
+    setTimeout(() => {
+      const rootEl = rootRef.current
+      const focusedEl = document.activeElement
+      const focusInside = rootEl && focusedEl && rootEl.contains(focusedEl)
+
+      if (!focusInside) {
+        // hide listbox
+        setFocused(false)
+      }
+    }, 0)
+  }, [])
+
+  const handleRootKeyDown = useCallback(
+    (event: React.KeyboardEvent<HTMLElement>) => {
+      if (event.key === 'ArrowDown') {
+        event.preventDefault()
+        if (!optionsLen) return
+        setSelectedIndex((index) => (index + 1) % optionsLen)
+
+        return
+      }
+
+      if (event.key === 'ArrowUp') {
+        event.preventDefault()
+        if (!optionsLen) return
+        setSelectedIndex((index) => {
+          return index === -1 ? optionsLen - 1 : (optionsLen + index - 1) % optionsLen
+        })
+
+        return
+      }
+
+      if (event.key === 'Escape') {
+        if (onChange) onChange('')
+        setFocused(false)
+        valueRef.current = ''
+        setValue('')
+
+        inputRef.current?.focus()
+
+        return
+      }
+
+      const target = event.target as Node
+      const listEl = listRef.current
+
+      // console.log(listEl?.contains(target))
+
+      if (
+        (listEl === target || listEl?.contains(target)) &&
+        !LIST_IGNORE_KEYS.includes(event.key)
+      ) {
+        inputRef.current?.focus()
+
+        return
+      }
+    },
+    [onChange, optionsLen]
+  )
 
   const handleInputChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -99,74 +174,18 @@ export function Autocomplete(props: Props) {
     [onChange]
   )
 
+  const handleInputFocus = useCallback(() => setFocused(true), [])
+
   const handleClearButtonClick = useCallback(() => {
     valueRef.current = ''
     setValue('')
     if (onChange) onChange('')
+    inputRef.current?.focus()
   }, [onChange])
 
-  const handleInputFocus = useCallback(() => {
-    setFocused(true)
-  }, [])
+  const handleClearButtonFocus = useCallback(() => setFocused(true), [])
 
-  const handleInputBlur = useCallback(() => {
-    if (!listHovered) setFocused(false)
-  }, [listHovered])
-
-  useEffect(() => {
-    if (valueProp !== valueRef.current) {
-      valueRef.current = valueProp
-      setValue(valueProp)
-    }
-  }, [valueProp])
-
-  const handleListMouseEnter = useCallback(() => {
-    setListHovered(true)
-  }, [])
-
-  const handleListMouseLeave = useCallback(() => {
-    setListHovered(false)
-  }, [])
-
-  const handleInputKeyDown = useCallback(
-    (event: React.KeyboardEvent<HTMLInputElement>) => {
-      if (event.key === 'ArrowDown') {
-        event.preventDefault()
-        if (!optionsLen) return
-        setListHovered(true)
-        setSelectedIndex((index) => {
-          return (index + 1) % optionsLen
-        })
-
-        return
-      }
-
-      if (event.key === 'ArrowUp') {
-        event.preventDefault()
-        if (!optionsLen) return
-        setListHovered(true)
-        setSelectedIndex((index) => {
-          return index === -1 ? optionsLen - 1 : (optionsLen + index - 1) % optionsLen
-        })
-
-        return
-      }
-
-      if (event.key === 'Enter') {
-        return
-      }
-
-      if (event.key === 'Escape') {
-        if (onChange) onChange('')
-        setFocused(false)
-      }
-
-      inputRef.current?.focus()
-    },
-    [onChange, optionsLen]
-  )
-
-  const handleSelect = useCallback(
+  const handleOptionSelect = useCallback(
     (v: string) => {
       if (onSelect) onSelect(v)
       if (onChange) onChange('')
@@ -175,8 +194,19 @@ export function Autocomplete(props: Props) {
     [onChange, onSelect]
   )
 
+  // Change the value when `value` prop changes
+  useEffect(() => {
+    if (valueProp !== valueRef.current) {
+      valueRef.current = valueProp
+      setValue(valueProp)
+    }
+  }, [valueProp])
+
+  // Reset selected item when the list changes
+  // @todo: what if the list changed, but the items have changed?
   useEffect(() => setSelectedIndex(-1), [optionsLen])
 
+  // Focus the selected item
   useEffect(() => {
     const listElement = listRef.current
 
@@ -190,7 +220,7 @@ export function Autocomplete(props: Props) {
   }, [selectedIndex])
 
   return (
-    <Root onKeyDown={handleInputKeyDown}>
+    <Root onBlur={handleRootBlur} onKeyDown={handleRootKeyDown} ref={rootRef}>
       <TextInput
         {...restProps}
         aria-activedescendant={activeItemId}
@@ -203,7 +233,6 @@ export function Autocomplete(props: Props) {
         border={border}
         icon="search"
         id={inputId}
-        onBlur={handleInputBlur}
         onChange={handleInputChange}
         onFocus={handleInputFocus}
         padding={padding}
@@ -216,9 +245,10 @@ export function Autocomplete(props: Props) {
       />
 
       {value.length > 0 && (
-        <ClearButtonBox margin={padding.map((v) => v - 1)}>
+        <ClearButtonBox margin={padding.map((v) => v - 1)} onFocus={handleClearButtonFocus}>
           <Button
             aria-label="Clear"
+            data-qa="clear-button"
             icon="close"
             mode="bleed"
             onClick={handleClearButtonClick}
@@ -228,22 +258,18 @@ export function Autocomplete(props: Props) {
         </ClearButtonBox>
       )}
 
-      <ListBoxContainer
-        hidden={!expanded}
-        onMouseEnter={handleListMouseEnter}
-        onMouseLeave={handleListMouseLeave}
-      >
+      <ListBoxContainer hidden={!expanded} onInput={console.log}>
         <ListBoxCard paddingY={1} radius={1} shadow={2}>
           <ul aria-multiselectable={false} id={listboxId} ref={listRef} role="listbox">
             {options.map((option, optionIndex) => (
               <AutosuggestOption
                 id={`${id}-option-${optionIndex}`}
                 key={option.value}
-                onSelect={handleSelect}
+                onSelect={handleOptionSelect}
                 selected={optionIndex === selectedIndex}
                 value={option.value}
               >
-                {renderOption(option)}
+                {cloneElement(renderOption(option), {tabIndex: -1})}
               </AutosuggestOption>
             ))}
           </ul>
