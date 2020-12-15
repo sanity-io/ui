6cb104b2910738542719d4acc4d413d3c96a8ee7
Marius Lundgård <studio@mariuslundgard.com>
2020-12-15 14:01:01 +0100

    [ui] Improve element helpers

--- ui/src/helpers/element.ts
+++ ui/src/helpers/element.ts
@@ -1,23 +1,23 @@
-export function isHTMLElement(node: Node): node is HTMLElement {
-  return node.nodeType === Node.ELEMENT_NODE
+export function isHTMLElement(node: unknown): node is HTMLElement {
+  return node instanceof Node && node.nodeType === Node.ELEMENT_NODE
 }
 
-export function isHTMLAnchorElement(element: HTMLElement): element is HTMLAnchorElement {
-  return element.nodeName === 'A'
+export function isHTMLAnchorElement(element: unknown): element is HTMLAnchorElement {
+  return isHTMLElement(element) && element.nodeName === 'A'
 }
 
-export function isHTMLInputElement(element: HTMLElement): element is HTMLInputElement {
-  return element.nodeName === 'INPUT'
+export function isHTMLInputElement(element: unknown): element is HTMLInputElement {
+  return isHTMLElement(element) && element.nodeName === 'INPUT'
 }
 
-export function isHTMLButtonElement(element: HTMLElement): element is HTMLButtonElement {
-  return element.nodeName === 'BUTTON'
+export function isHTMLButtonElement(element: unknown): element is HTMLButtonElement {
+  return isHTMLElement(element) && element.nodeName === 'BUTTON'
 }
 
-export function isHTMLSelectElement(element: HTMLElement): element is HTMLSelectElement {
-  return element.nodeName === 'SELECT'
+export function isHTMLSelectElement(element: unknown): element is HTMLSelectElement {
+  return isHTMLElement(element) && element.nodeName === 'SELECT'
 }
 
-export function isHTMLTextAreaElement(element: HTMLElement): element is HTMLTextAreaElement {
-  return element.nodeName === 'TEXTAREA'
+export function isHTMLTextAreaElement(element: unknown): element is HTMLTextAreaElement {
+  return isHTMLElement(element) && element.nodeName === 'TEXTAREA'
 }
