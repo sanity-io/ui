2dfe7ba16d4aa710479abad61c91b96c862c31ac
Marius Lundgård <studio@mariuslundgard.com>
2020-12-15 18:46:16 +0100

    [studio] Update screen schema type

--- studio/base/schema/screen/screen.js
+++ studio/base/schema/screen/screen.js
@@ -5,6 +5,58 @@ const arcadeSection = {
   fields: [{type: 'boolean', name: 'test', title: 'Test'}],
 }
 
+const heroSection = {
+  type: 'object',
+  name: 'screenSection.hero',
+  title: 'Hero',
+  fields: [
+    {
+      type: 'string',
+      name: 'headline',
+      title: 'Headline',
+    },
+    {
+      type: 'string',
+      name: 'copy',
+      title: 'Copy',
+    },
+    {
+      type: 'string',
+      name: 'linksHeader',
+      title: 'Links header',
+    },
+    {
+      type: 'array',
+      name: 'links',
+      title: 'Links',
+      of: [
+        {
+          type: 'object',
+          name: 'link',
+          title: 'Link',
+          fields: [
+            {
+              type: 'string',
+              name: 'title',
+              title: 'Title',
+            },
+            {
+              type: 'text',
+              name: 'subtitle',
+              title: 'Subtitle',
+            },
+            {
+              type: 'string',
+              name: 'href',
+              title: 'Hypertext reference',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
 // const colorGridSection = {
 //   type: 'object',
 //   name: 'screenSection.colorGrid',
@@ -14,6 +66,7 @@ const arcadeSection = {
 
 export const screenSections = [
   arcadeSection,
+  heroSection,
   // colorGridSection
 ]
 
@@ -29,6 +82,7 @@ export const screen = {
       title: 'Sections',
       of: [
         {type: 'screenSection.arcade'},
+        {type: 'screenSection.hero'},
         // {type: 'screenSection.colorGrid'}
       ],
     },
