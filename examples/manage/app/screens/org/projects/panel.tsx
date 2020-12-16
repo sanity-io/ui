44d129f18390751e6927b88fa5ae08c2423f4c9e
Marius Lundgård <studio@mariuslundgard.com>
2020-12-16 11:37:27 +0100

    [examples] Update manage example

--- examples/manage/app/screens/org/projects/panel.tsx
+++ examples/manage/app/screens/org/projects/panel.tsx
@@ -1,4 +1,16 @@
-import {Box, Card, Container, Heading} from '@sanity/ui'
+import {EllipsisVerticalIcon} from '@sanity/icons'
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuItem,
+  Stack,
+} from '@sanity/ui'
 import React from 'react'
 
 export function ProjectsPanel() {
@@ -8,7 +20,74 @@ export function ProjectsPanel() {
         <Box paddingX={4} paddingY={6}>
           <Heading>Projects</Heading>
         </Box>
+
+        <Stack padding={4} space={4}>
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+          <ProjectItem id="foo" />
+        </Stack>
       </Container>
+    </Card>
+  )
+}
+
+function ProjectItem({id}: {id: string}) {
+  return (
+    <Card radius={3} shadow={1}>
+      <Flex align="center">
+        <Box flex={1} paddingX={4} paddingY={3}>
+          <Heading>Project: {id}</Heading>
+        </Box>
+        <Box padding={2}>
+          <MenuButton
+            id={`${id}-menu`}
+            button={<Button icon={EllipsisVerticalIcon} mode="bleed" />}
+            menu={
+              <Menu>
+                <MenuItem text="Foo" />
+                <MenuItem text="Foo" />
+                <MenuItem text="Foo" />
+                <MenuItem text="Foo" />
+              </Menu>
+            }
+          />
+        </Box>
+      </Flex>
     </Card>
   )
 }
