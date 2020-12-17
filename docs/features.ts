6475ffd3759971715e1b0d991f7df208ffffd3ea
Marius Lundgård <studio@mariuslundgard.com>
2020-12-17 19:22:57 +0100

    [docs] Improve feature config

--- docs/features.ts
+++ docs/features.ts
@@ -1,11 +1,32 @@
+const __DEV__ = process.env.NODE_ENV === 'development'
+
 /**
  * Fetch draft data when `true`.
  *
- * Toggle this flag on/off by setting the environment variable `FEATURE_PREVIEW=true|false`.
+ * Toggle this flag on/off by setting the environment variable `NEXT_PUBLIC_FEATURE_PREVIEW=true|false`.
  * Defaults to `true` in development.
  */
-export const PREVIEW: boolean = process.env.FEATURE_PREVIEW
-  ? JSON.parse(process.env.FEATURE_PREVIEW)
-  : process.env.NODE_ENV === 'development'
+export const PREVIEW = getFlag(process.env.NEXT_PUBLIC_FEATURE_PREVIEW, __DEV__)
 
-export const HINT_HIDDEN_CONTENT = process.env.NODE_ENV === 'development'
+/**
+ * Show hints of hidden content when `true`.
+ *
+ * Toggle this flag on/off by setting the environment variable `NEXT_PUBLIC_FEATURE_HINT_HIDDEN_CONTENT=true|false`.
+ * Defaults to `true` in development.
+ */
+export const HINT_HIDDEN_CONTENT = getFlag(
+  process.env.NEXT_PUBLIC_FEATURE_HINT_HIDDEN_CONTENT,
+  __DEV__
+)
+
+function getFlag<T = unknown>(envValue: string | undefined, defaultValue: T) {
+  try {
+    if (typeof envValue === 'string') {
+      return JSON.parse(envValue) as T
+    }
+  } catch (_) {
+    console.warn(`features: could not parse env value`)
+  }
+
+  return defaultValue
+}
