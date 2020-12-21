12bd3b79e8d471b7313d1d548fbca442ca3325db
Marius Lundgård <studio@mariuslundgard.com>
2020-12-21 14:40:30 +0100

    [ui] Improve usePrefersDark

--- ui/src/hooks/usePrefersDark.ts
+++ ui/src/hooks/usePrefersDark.ts
@@ -1,16 +1,18 @@
 import {useEffect, useState} from 'react'
 
 export function usePrefersDark() {
-  const [dark, setDark] = useState(false)
+  const [dark, setDark] = useState(() =>
+    typeof window !== 'undefined'
+      ? window.matchMedia('(prefers-color-scheme: dark)').matches
+      : false
+  )
 
   useEffect(() => {
     const mq = window.matchMedia('(prefers-color-scheme: dark)')
 
     setDark(mq.matches)
 
-    const handleChange = () => {
-      setDark(mq.matches)
-    }
+    const handleChange = () => setDark(mq.matches)
 
     mq.addEventListener('change', handleChange)
 
