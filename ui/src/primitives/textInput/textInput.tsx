7fe35ca4419e4510eb7aeabc1b3b42c002084a96
Marius Lundgård <studio@mariuslundgard.com>
2021-01-18 13:58:24 +0100

    [ui] Add clear button to TextInput

--- ui/src/primitives/textInput/textInput.tsx
+++ ui/src/primitives/textInput/textInput.tsx
@@ -1,7 +1,9 @@
-import React, {createElement, forwardRef, isValidElement} from 'react'
+import {CloseIcon} from '@sanity/icons'
+import React, {createElement, forwardRef, isValidElement, useCallback} from 'react'
 import {isValidElementType} from 'react-is'
 import styled from 'styled-components'
 import {useForwardedRef, useCustomValidity} from '../../hooks'
+import {getResponsiveProp} from '../../styles'
 import {
   responsiveRadiusStyle,
   ResponsiveRadiusStyleProps,
@@ -13,15 +15,26 @@ import {
 } from '../../styles/internal'
 import {ThemeFontWeightKey} from '../../theme'
 import {Box} from '../box'
+import {Button, ButtonProps} from '../button'
 import {Card} from '../card'
 import {Text} from '../text'
 
 interface TextInputProps {
   border?: boolean
+  /**
+   * @beta
+   */
+  clearButton?:
+    | boolean
+    | (Omit<ButtonProps, 'as'> & Omit<React.HTMLProps<HTMLButtonElement>, 'as' | 'ref'>)
   customValidity?: string
   fontSize?: number | number[]
   icon?: React.ComponentType | React.ReactNode
   iconRight?: React.ComponentType | React.ReactNode
+  /**
+   * @beta
+   */
+  onClear?: () => void
   padding?: number | number[]
   prefix?: React.ReactNode
   radius?: number | number[]
@@ -81,13 +94,13 @@ const Presentation = styled.span<ResponsiveRadiusStyleProps & TextInputRepresent
   textInputStyle.representation
 )
 
-const IconLeftBox = styled(Box)`
+const LeftBox = styled(Box)`
   position: absolute;
   top: 0;
   left: 0;
 `
 
-const IconRightBox = styled(Box)`
+const RightBox = styled(Box)`
   position: absolute;
   top: 0;
   right: 0;
@@ -100,11 +113,13 @@ export const TextInput = forwardRef(
   ) => {
     const {
       border = true,
+      clearButton,
       disabled = false,
       fontSize = 2,
       icon,
       iconRight,
-      padding = 3,
+      onClear,
+      padding: paddingProp = 3,
       prefix,
       radius = 1,
       space = 3,
@@ -116,7 +131,28 @@ export const TextInput = forwardRef(
 
     const ref = useForwardedRef(forwardedRef)
 
+    const padding = getResponsiveProp(paddingProp)
+
     useCustomValidity(ref, customValidity)
+
+    // Prevent the clear button from taking the focus away from the input
+    const handleClearMouseDown = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault()
+      event.stopPropagation()
+    }, [])
+
+    const handleClearClick = useCallback(
+      (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        event.stopPropagation()
+
+        if (onClear) onClear()
+
+        // Focus the input, in case focus has been lost when clicking the clear button
+        ref.current?.focus()
+      },
+      [onClear, ref]
+    )
 
     return (
       <Root data-ui="TextInput">
@@ -131,7 +167,7 @@ export const TextInput = forwardRef(
             data-as="input"
             {...restProps}
             $iconLeft={Boolean(icon)}
-            $iconRight={Boolean(iconRight)}
+            $iconRight={Boolean(iconRight) || Boolean(clearButton)}
             $padding={padding}
             $space={space}
             $fontSize={fontSize}
@@ -147,23 +183,38 @@ export const TextInput = forwardRef(
             $radius={radius}
           >
             {icon && (
-              <IconLeftBox padding={padding}>
+              <LeftBox padding={padding}>
                 <Text size={fontSize}>
                   {isValidElement(icon) && icon}
                   {isValidElementType(icon) && createElement(icon)}
                 </Text>
-              </IconLeftBox>
+              </LeftBox>
             )}
 
-            {iconRight && (
-              <IconRightBox padding={padding}>
+            {!clearButton && iconRight && (
+              <RightBox padding={padding}>
                 <Text size={fontSize}>
                   {isValidElement(iconRight) && iconRight}
                   {isValidElementType(iconRight) && createElement(iconRight)}
                 </Text>
-              </IconRightBox>
+              </RightBox>
             )}
           </Presentation>
+
+          {clearButton && (
+            <RightBox padding={padding.map((v) => v - 2)} style={{zIndex: 2}}>
+              <Button
+                {...(typeof clearButton === 'object' ? clearButton : {})}
+                data-qa="clear-button"
+                fontSize={fontSize}
+                icon={CloseIcon}
+                mode="bleed"
+                onClick={handleClearClick}
+                onMouseDown={handleClearMouseDown}
+                padding={padding.map((v) => v - 1)}
+              />
+            </RightBox>
+          )}
         </InputRoot>
 
         {suffix && (
