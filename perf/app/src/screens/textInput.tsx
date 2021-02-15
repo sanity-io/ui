125ebf6ce9fae2d956d8b5dfb4c81b02a25d9191
Marius Lundgård <studio@mariuslundgard.com>
2021-02-15 17:22:36 +0100

    [perf] Expand TextInput test

--- perf/app/src/screens/textInput.tsx
+++ perf/app/src/screens/textInput.tsx
@@ -1,16 +1,46 @@
-import {Card, TextInput} from '@sanity/ui'
-import React, {useState} from 'react'
+import {Card, TextInput as UITextInput} from '@sanity/ui'
+import React, {createElement, useCallback, useState} from 'react'
+import styled from 'styled-components'
+
+const StyledTextInput = styled.input`
+  background-color: ${({theme}) => theme.sanity.color.base.bg};
+`
+
+const COMPONENT_TYPE = 'ui'
+
+const components: any = {
+  native: 'input',
+  styled: StyledTextInput,
+  ui: UITextInput,
+}
+
+const component = components[COMPONENT_TYPE]
 
 export function TextInputScreen() {
   const [value, setValue] = useState('')
+  const [focused, setFocused] = useState(false)
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleBlur = useCallback(() => {
+    setFocused(false)
+  }, [])
+
+  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.currentTarget.value)
-  }
+  }, [])
+
+  const handleFocus = useCallback(() => {
+    setFocused(true)
+  }, [])
 
   return (
-    <Card padding={4}>
-      <TextInput data-test="text-input" onChange={handleChange} value={value} />
+    <Card data-focused={focused} padding={4}>
+      {createElement(component, {
+        'data-test': 'text-input',
+        onBlur: handleBlur,
+        onChange: handleChange,
+        onFocus: handleFocus,
+        value,
+      })}
     </Card>
   )
 }
