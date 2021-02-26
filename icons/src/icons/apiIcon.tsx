510f6bdfbd3e6a97bb10ab349f4ff399a61d049e
Fred Carlsen <fred@sjelfull.no>
2021-02-26 14:29:06 +0100

    feat(icons): Add missing manage icons (#441)

---
+++ icons/src/icons/apiIcon.tsx
@@ -0,0 +1,30 @@
+/*
+ * AUTO-GENERATED, DO NOT EDIT
+ */
+
+import React from 'react'
+
+function ApiIcon(props: React.SVGProps<SVGSVGElement>, svgRef?: React.Ref<SVGSVGElement>) {
+  return (
+    <svg
+      data-sanity-icon="api"
+      width="1em"
+      height="1em"
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={svgRef}
+      {...props}
+    >
+      <path
+        d="M12.5 16V12.5M20 9V16M12.5 12.5V9H16.5V12.5H12.5ZM5 16V15.5L5.66667 13M10 16V15.5L9.33333 13M5.66667 13L7 9H8L9.33333 13M5.66667 13H9.33333"
+        stroke="currentColor"
+        strokeWidth={1.2}
+        strokeLinecap="square"
+      />
+    </svg>
+  )
+}
+
+const ForwardRef = React.forwardRef(ApiIcon)
+export default ForwardRef
