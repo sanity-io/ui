4f97c5afcfbc042d70a4624f587996a3a5d978fa
Fred Carlsen <fred@sjelfull.no>
2021-02-28 13:26:53 +0100

    feat(icons): Add copy icon

---
+++ icons/src/icons/copyIcon.tsx
@@ -0,0 +1,29 @@
+/*
+ * AUTO-GENERATED, DO NOT EDIT
+ */
+
+import React from 'react'
+
+function CopyIcon(props: React.SVGProps<SVGSVGElement>, svgRef?: React.Ref<SVGSVGElement>) {
+  return (
+    <svg
+      data-sanity-icon="copy"
+      width="1em"
+      height="1em"
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={svgRef}
+      {...props}
+    >
+      <path
+        d="M8.5 8H5.5V20H16.5V17M19.5 5H8.5V17H19.5V5Z"
+        stroke="currentColor"
+        strokeWidth={1.2}
+      />
+    </svg>
+  )
+}
+
+const ForwardRef = React.forwardRef(CopyIcon)
+export default ForwardRef
