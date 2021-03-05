a7f4e5e12cd5cf1c57d35346506a0c0576f757e4
Marius Lundgård <studio@mariuslundgard.com>
2021-03-05 13:22:11 +0100

    docs(ui): make comment TSDoc-compliant

--- ui/src/hooks/useResponsiveProp.ts
+++ ui/src/hooks/useResponsiveProp.ts
@@ -2,7 +2,8 @@ import {useMemo} from 'react'
 import {getResponsiveProp} from '../styles'
 
 /**
- * @beta This API might change. DO NOT USE IN PRODUCTION.
+ * This API might change. DO NOT USE IN PRODUCTION.
+ * @beta
  */
 export function useResponsiveProp<T = number>(val: T | T[] | undefined, defaultVal?: T[]): T[] {
   return useMemo(() => getResponsiveProp(val, defaultVal), [defaultVal, val])
