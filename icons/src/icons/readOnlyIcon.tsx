967a7913c8f0466808d104d961ffe90535764b98
Bjørge Næss <bjoerge@gmail.com>
2021-05-11 15:58:31 +0200

    feat(icons): add read only icon (#766)

---
+++ icons/src/icons/readOnlyIcon.tsx
@@ -0,0 +1,29 @@
+/*
+ * AUTO-GENERATED, DO NOT EDIT
+ */
+
+import React from 'react'
+
+function ReadOnlyIcon(props: React.SVGProps<SVGSVGElement>, svgRef?: React.Ref<SVGSVGElement>) {
+  return (
+    <svg
+      data-sanity-icon="read-only"
+      width="1em"
+      height="1em"
+      viewBox="0 0 17 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={svgRef}
+      {...props}
+    >
+      <path
+        d="M11 3L14 6M6 8L3 11L2 15L6 14L9 11M8 6L13 1L16 4L11 9M15 15L1 1"
+        stroke="currentColor"
+        strokeWidth={1.2}
+      />
+    </svg>
+  )
+}
+
+const ForwardRef = React.forwardRef(ReadOnlyIcon)
+export default ForwardRef
