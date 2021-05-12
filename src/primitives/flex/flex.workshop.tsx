264705ac55b151879ff72996117edd3a054b3673
Marius Lundgård <studio@mariuslundgard.com>
2021-05-12 23:30:15 +0200

    feat(ui): add `gap` property to `Flex`

--- src/primitives/flex/flex.workshop.tsx
+++ src/primitives/flex/flex.workshop.tsx
@@ -1,4 +1,4 @@
-import {Card, Code, Flex} from '@sanity/ui'
+import {Card, Code, Flex, Text} from '@sanity/ui'
 import {defineScope, useSelect} from '@sanity/ui-workshop'
 import React from 'react'
 import styled from 'styled-components'
@@ -10,6 +10,7 @@ const FLEX_DIRECTION_OPTIONS: {[key: string]: 'row' | 'column'} = {
 
 export default defineScope('primitives/flex', 'Flex', [
   {name: 'plain', title: 'Plain', component: PlainStory},
+  {name: 'gap', title: 'Gap', component: GapStory},
 ])
 
 const DebugCard = styled(Card)`
@@ -39,6 +40,30 @@ function PlainStory() {
       <DebugCard flex={1}>
         <Code>1</Code>
       </DebugCard>
+    </Flex>
+  )
+}
+
+function GapStory() {
+  return (
+    <Flex align="center" height="fill" justify="center" padding={[4, 5, 6]} sizing="border">
+      <Flex align="center" gap={[2, 3, 4]} justify="center" wrap="wrap">
+        <Card padding={3} scheme="dark">
+          <Text size={0}>Card 0</Text>
+        </Card>
+        <Card padding={3} scheme="dark">
+          <Text size={1}>Card 1</Text>
+        </Card>
+        <Card padding={3} scheme="dark">
+          <Text size={2}>Card 2</Text>
+        </Card>
+        <Card padding={3} scheme="dark">
+          <Text size={3}>Card 3</Text>
+        </Card>
+        <Card padding={3} scheme="dark">
+          <Text size={4}>Card 4</Text>
+        </Card>
+      </Flex>
     </Flex>
   )
 }
