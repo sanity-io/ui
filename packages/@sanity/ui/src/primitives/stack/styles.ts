c30addcb638ae57f04bb6306fec8cd4a98acb260
Marius Lundgård <studio@mariuslundgard.com>
2021-05-14 11:01:24 +0200

    fix(ui): revert `Inline` and `Stack`

---
+++ packages/@sanity/ui/src/primitives/stack/styles.ts
@@ -0,0 +1,27 @@
+import {getResponsiveProp, rem, responsive, ThemeProps} from '../../styles'
+
+export interface ResponsiveStackSpaceStyleProps {
+  $space?: number | number[]
+}
+
+export function stackBaseStyle() {
+  return {
+    '&:not([hidden])': {
+      display: 'grid',
+    },
+    '&[data-as="ul"],&[data-as="ol"]': {
+      listStyle: 'none',
+    },
+    gridTemplateColumns: 'minmax(0, 1fr)',
+    gridAutoRows: 'min-content',
+  }
+}
+
+export function responsiveStackSpaceStyle(props: ResponsiveStackSpaceStyleProps & ThemeProps) {
+  const {theme} = props
+  const {media, space} = theme.sanity
+
+  return responsive(media, getResponsiveProp(props.$space), (spaceIndex) => ({
+    gridGap: rem(space[spaceIndex]),
+  }))
+}
