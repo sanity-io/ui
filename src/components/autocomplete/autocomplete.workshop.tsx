b101bff6a8a924b8dd52372395f0c569024a3c3e
Marius Lundgård <studio@mariuslundgard.com>
2021-05-20 13:32:35 +0200

    fix: disabled inputs clear button and open button in restricted mode

--- src/components/autocomplete/autocomplete.workshop.tsx
+++ src/components/autocomplete/autocomplete.workshop.tsx
@@ -57,11 +57,14 @@ const RADIUS_OPTIONS = {
 
 export function DefaultStory() {
   const options = countries.map((country) => ({value: country.code}))
+  const border = useBoolean('Border', true, 'Props')
   const customValidity = useText('Custom validity', '', 'Props') || undefined
+  const disabled = useBoolean('Disabled', false, 'Props')
   const fontSize = Number(useSelect('Font size', TEXT_SIZE_OPTIONS, '2', 'Props'))
   const openButton = useBoolean('Open button', false, 'Props')
   const padding = useSelect('Padding', SPACE_OPTIONS, 3, 'Props')
   const radius = Number(useSelect('Radius', RADIUS_OPTIONS, '2', 'Props'))
+  const readOnly = useBoolean('Read only', false, 'Props')
 
   return (
     <Box padding={[4, 5, 6]}>
@@ -70,13 +73,16 @@ export function DefaultStory() {
           Country code
         </Text>
         <Autocomplete
+          border={border}
           customValidity={customValidity}
+          disabled={disabled}
           fontSize={fontSize}
           id="default"
           openButton={openButton}
           options={options}
           padding={padding}
           radius={radius}
+          readOnly={readOnly}
         />
       </Stack>
     </Box>
@@ -85,9 +91,11 @@ export function DefaultStory() {
 
 function CustomStory() {
   const data: ExampleOption[] = countries.map((d) => ({value: d.code, title: d.name}))
-  const border = useBoolean('Border', true, 'Props') || false
+  const border = useBoolean('Border', true, 'Props')
+  const disabled = useBoolean('Disabled', false, 'Props')
   const fontSize = Number(useSelect('Font size', TEXT_SIZE_OPTIONS, '2', 'Props'))
   const radius = Number(useSelect('Radius', RADIUS_OPTIONS, '2', 'Props'))
+  const readOnly = useBoolean('Read only', false, 'Props')
   const [value, setValue] = useState('')
 
   const renderOption = useCallback((option: ExampleOption) => {
@@ -124,12 +132,14 @@ function CustomStory() {
         <Autocomplete
           aria-describedby="custom-label"
           border={border}
+          disabled={disabled}
           filterOption={filterOption}
           fontSize={fontSize}
           id="custom"
           onChange={setValue}
           options={options}
           radius={radius}
+          readOnly={readOnly}
           renderOption={renderOption}
           renderValue={renderValue}
           value={value}
