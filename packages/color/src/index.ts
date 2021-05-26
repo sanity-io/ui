b6042c1b78a1c29459c51eabe497a08c2410fd7b
Marius Lundgård <studio@mariuslundgard.com>
2021-05-26 21:34:03 +0200

    refactor: fix API Extractor errors

--- packages/color/src/index.ts
+++ packages/color/src/index.ts
@@ -1,11 +1,8 @@
 import {black as blackHex, white as whiteHex} from './config'
-import * as hues from './hues'
 
 export * from './hues'
 export * from './constants'
 export * from './types'
-
-export {hues}
 
 export const black = {title: 'Black', hex: blackHex}
 export const white = {title: 'White', hex: whiteHex}
