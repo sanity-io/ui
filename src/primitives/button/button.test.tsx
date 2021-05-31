4b8a26ea59ac8e3c79530193e0926c12c195cdb7
Marius Lundgård <studio@mariuslundgard.com>
2021-05-31 13:49:24 +0200

    fix(ui): improve typings of `ButtonProps`

--- src/primitives/button/button.test.tsx
+++ src/primitives/button/button.test.tsx
@@ -3,10 +3,10 @@ import {screen} from '@testing-library/react'
 import {axe} from 'jest-axe'
 import React from 'react'
 import {render} from '../../../test'
-import {Button} from './button'
+import {Button, ButtonProps} from './button'
 
 describe('atoms/button', () => {
-  it('should have no violations (axe)', async () => {
+  it('Axe: should have no violations', async () => {
     const lightResult = render(<Button icon={AddIcon} text="Label" tone="positive" />, {
       scheme: 'light',
     })
@@ -24,5 +24,17 @@ describe('atoms/button', () => {
     render(<Button text="Button text" />)
 
     expect(screen.getByText('Button text')).toBeInTheDocument()
+  })
+
+  it('should wrap button', () => {
+    function WrappedButton({button: buttonProps}: {button: ButtonProps}) {
+      return <Button {...buttonProps} />
+    }
+
+    const buttonProps: ButtonProps = {
+      text: 'Button text',
+    }
+
+    render(<WrappedButton button={buttonProps} />)
   })
 })
