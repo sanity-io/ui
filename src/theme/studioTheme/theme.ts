19eb13b7b0a050ad00ae1a0a36eb45bcb8ff35f2
Marius Lundgård <studio@mariuslundgard.com>
2021-06-02 12:39:10 +0200

    feat(ui): add `ThemeLayer` type and property

--- src/theme/studioTheme/theme.ts
+++ src/theme/studioTheme/theme.ts
@@ -27,16 +27,10 @@ export const studioTheme: RootTheme = {
   radius: [0, 1, 3, 6, 9, 12, 21],
   shadows: [
     null,
-    // 1
-    // {umbra: [0, 2, 1, -1], penumbra: [0, 1, 1, 0], ambient: [0, 1, 3, 0]},
     {umbra: [0, 0, 0, 0], penumbra: [0, 0, 0, 0], ambient: [0, 0, 0, 0]},
-    // 6
     {umbra: [0, 3, 5, -1], penumbra: [0, 6, 10, 0], ambient: [0, 1, 18, 0]},
-    // 12
     {umbra: [0, 7, 8, -4], penumbra: [0, 12, 17, 2], ambient: [0, 5, 22, 4]},
-    // 18
     {umbra: [0, 9, 11, -5], penumbra: [0, 18, 28, 2], ambient: [0, 7, 34, 6]},
-    // 24
     {umbra: [0, 11, 15, -7], penumbra: [0, 24, 38, 3], ambient: [0, 9, 46, 8]},
   ],
   space: [0, 4, 8, 12, 20, 32, 52, 84, 136, 220],
