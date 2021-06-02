6aa315356443646b56b158d4a2e62a2af8664374
Marius Lundgård <studio@mariuslundgard.com>
2021-06-02 12:39:10 +0200

    feat(ui): add `ThemeLayer` type and property

--- packages/@sanity/ui/src/theme/themeProvider.tsx
+++ packages/@sanity/ui/src/theme/themeProvider.tsx
@@ -1,5 +1,6 @@
 import React, {useMemo} from 'react'
 import {ThemeProvider as StyledThemeProvider} from 'styled-components'
+import {DEFAULT_THEME_LAYER} from './defaults'
 import {ThemeColorSchemeKey, ThemeColorName} from './lib/theme'
 import {ThemeContext} from './themeContext'
 import {RootTheme, Theme, ThemeContextValue} from './types'
@@ -21,11 +22,12 @@ export function ThemeProvider(props: ThemeProviderProps): React.ReactElement {
   const {children, scheme = 'light', theme: rootTheme, tone = 'default'} = props
 
   const theme: Theme = useMemo(() => {
-    const {color: rootColor, ...restTheme} = rootTheme
+    const {color: rootColor, layer: rootLayer, ...restTheme} = rootTheme
     const colorScheme = rootColor[scheme] || rootColor.light
     const color = colorScheme[tone] || colorScheme.default
+    const layer = rootLayer || DEFAULT_THEME_LAYER
 
-    return {sanity: {...restTheme, color}}
+    return {sanity: {...restTheme, color, layer}}
   }, [rootTheme, scheme, tone])
 
   const value: ThemeContextValue = useMemo(
