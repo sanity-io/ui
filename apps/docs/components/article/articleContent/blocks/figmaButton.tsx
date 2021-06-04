91b5a684d253d68d1d1ad391b943c342673d1335
Marius Lundgård <studio@mariuslundgard.com>
2021-06-04 13:12:46 +0200

    fix: remove extra padding

--- apps/docs/components/article/articleContent/blocks/figmaButton.tsx
+++ apps/docs/components/article/articleContent/blocks/figmaButton.tsx
@@ -1,4 +1,4 @@
-import {Button, Flex, Box, Text} from '@sanity/ui'
+import {Button} from '@sanity/ui'
 import React from 'react'
 import {FigmaLogo} from '$components/assets/figmaLogo'
 
@@ -10,15 +10,14 @@ export function FigmaButton(props: any) {
   }
 
   return (
-    <Button as="a" href={url} mode="ghost" rel="noreferrer" target="_blank">
-      <Flex padding={3}>
-        <Text size={2}>
-          <FigmaLogo />
-        </Text>
-        <Box marginLeft={3}>
-          <Text size={2}>{props.node.title}</Text>
-        </Box>
-      </Flex>
-    </Button>
+    <Button
+      as="a"
+      href={url}
+      icon={FigmaLogo}
+      mode="ghost"
+      rel="noreferrer"
+      target="_blank"
+      text={props.node.title}
+    />
   )
 }
