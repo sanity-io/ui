a212e4a905ac05e9fb55659141eada8488a628a6
Marius Lundgård <studio@mariuslundgard.com>
2021-06-04 13:37:24 +0200

    fix(examples): remove extra padding

--- examples/studio/src/$sanity/layout/navbar/presenceMenu.tsx
+++ examples/studio/src/$sanity/layout/navbar/presenceMenu.tsx
@@ -18,35 +18,29 @@ export function PresenceMenu() {
       id="members-menu"
       menu={
         <Menu>
-          <MenuItem>
-            <Box paddingX={3} paddingY={2}>
-              <Flex align="center">
-                <Box marginRight={3}>
-                  <Avatar color="cyan" initials="ir" size={1} />
-                </Box>
-                <Text>Ida Vikan Rise</Text>
-              </Flex>
-            </Box>
+          <MenuItem paddingX={3} paddingY={2}>
+            <Flex align="center">
+              <Box marginRight={3}>
+                <Avatar color="cyan" initials="ir" size={1} />
+              </Box>
+              <Text>Ida Vikan Rise</Text>
+            </Flex>
           </MenuItem>
-          <MenuItem>
-            <Box paddingX={3} paddingY={2}>
-              <Flex align="center">
-                <Box marginRight={3}>
-                  <Avatar color="blue" initials="ss" size={1} />
-                </Box>
-                <Text>Simen Svale Skogsrud</Text>
-              </Flex>
-            </Box>
+          <MenuItem paddingX={3} paddingY={2}>
+            <Flex align="center">
+              <Box marginRight={3}>
+                <Avatar color="blue" initials="ss" size={1} />
+              </Box>
+              <Text>Simen Svale Skogsrud</Text>
+            </Flex>
           </MenuItem>
-          <MenuItem>
-            <Box paddingX={3} paddingY={2}>
-              <Flex align="center">
-                <Box marginRight={3}>
-                  <Avatar color="purple" initials="eh" size={1} />
-                </Box>
-                <Text>Espen Hovlandsdal</Text>
-              </Flex>
-            </Box>
+          <MenuItem paddingX={3} paddingY={2}>
+            <Flex align="center">
+              <Box marginRight={3}>
+                <Avatar color="purple" initials="eh" size={1} />
+              </Box>
+              <Text>Espen Hovlandsdal</Text>
+            </Flex>
           </MenuItem>
         </Menu>
       }
