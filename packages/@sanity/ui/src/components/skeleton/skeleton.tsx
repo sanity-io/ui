92980aff792a70994f1ea415c7268a09df87d8d3
hermanwikner <wiknerherman@hotmail.com>
2021-06-10 16:06:36 +0200

    feat(ui): add text skeleton, styles and delay

--- packages/@sanity/ui/src/components/skeleton/skeleton.tsx
+++ packages/@sanity/ui/src/components/skeleton/skeleton.tsx
@@ -1,22 +1,48 @@
-import React, {forwardRef} from 'react'
+import React, {forwardRef, useEffect, useState} from 'react'
 import styled from 'styled-components'
-import {Box, BoxProps} from '../../primitives'
-import {ResponsiveRadiusProps} from '../../primitives/types'
+import {Box} from '../../primitives'
+import {BoxProps, ResponsiveRadiusProps} from '../../primitives'
 import {responsiveRadiusStyle, ResponsiveRadiusStyleProps} from '../../styles/internal'
+import {skeletonStyle} from './styles'
 
+const Root = styled(Box)<{$animated: boolean; $visible: boolean} & ResponsiveRadiusStyleProps>(
+  responsiveRadiusStyle,
+  skeletonStyle
+)
+
+/**
+ * This API might change. DO NOT USE IN PRODUCTION.
+ * @beta
+ */
 export interface SkeletonProps extends ResponsiveRadiusProps, Omit<BoxProps, 'children'> {
-  animationDuration?: number
+  animated?: boolean
+  delay?: number
 }
 
-const Root = styled(Box)<{$animationDuration: number} & ResponsiveRadiusStyleProps>`
-  background-color: var(--card-skeleton-color-from);
-  ${responsiveRadiusStyle}
-`
-
+/**
+ * This API might change. DO NOT USE IN PRODUCTION.
+ * @beta
+ */
 export const Skeleton = forwardRef(function Skeleton(
-  props: SkeletonProps & React.HTMLProps<HTMLDivElement>
+  props: SkeletonProps & React.HTMLProps<HTMLDivElement>,
+  ref: React.Ref<HTMLDivElement>
 ) {
-  const {animationDuration, radius, ...restProps} = props
+  const {animated = false, delay, radius, ...restProps} = props
+  const [visible, setVisible] = useState<boolean>(delay ? false : true)
 
-  return <Root {...restProps} $animationDuration={animationDuration} $radius={radius} />
+  useEffect(() => {
+    if (!delay) {
+      return setVisible(true)
+    }
+
+    const timeout = setTimeout(() => {
+      setVisible(true)
+    }, delay)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [delay])
+
+  return <Root {...restProps} $animated={animated} $radius={radius} $visible={visible} ref={ref} />
 })
--- packages/@sanity/ui/src/components/skeleton/skeleton.workshop.tsx
+++ packages/@sanity/ui/src/components/skeleton/skeleton.workshop.tsx
@@ -1,31 +1,129 @@
-import {Box, Card, Container, Flex, Skeleton, Stack} from '@sanity/ui'
-import {defineScope} from '@sanity/ui-workshop'
+import {
+  Box,
+  Card,
+  Text,
+  Container,
+  Skeleton,
+  TextSkeleton,
+  CodeSkeleton,
+  HeadingSkeleton,
+  LabelSkeleton,
+  Stack,
+  Flex,
+  ThemeColorToneKey,
+  Grid,
+} from '@sanity/ui'
+import {defineScope, useSelect, useBoolean} from '@sanity/ui-workshop'
 import React from 'react'
 
 export default defineScope('components/skeleton', 'Skeleton', [
-  {name: 'example', title: 'Example', component: ExampleStory},
+  {name: 'skeleton', title: 'Skeleton', component: SkeletonStory},
+  {name: 'skeleton-delay', title: 'Skeleton delay', component: SkeletonDelayStory},
 ])
 
-function ExampleStory() {
+const TONE_OPTIONS: {[key: string]: ThemeColorToneKey} = {
+  Default: 'default',
+  Transparent: 'transparent',
+  Primary: 'primary',
+  Positive: 'positive',
+  Caution: 'caution',
+  Critical: 'critical',
+}
+
+function SkeletonStory() {
+  const tone = useSelect('Tone', TONE_OPTIONS, '', 'Props') || 'default'
+  const animated = useBoolean('Animated', true)
+
   return (
     <Box padding={[4, 5, 6]}>
-      <Container width={0}>
-        <Card marginY={4} radius={2} shadow={1} tone="caution">
-          <Stack padding={4} space={4}>
-            <Skeleton style={{height: 100}} radius={2} />
-            <Stack space={3}>
-              <Skeleton style={{height: 11, width: '50%'}} />
-              <Skeleton style={{height: 9, width: '60%'}} />
-            </Stack>
-          </Stack>
-        </Card>
+      <Container width={1}>
+        <Stack space={4}>
+          {[1, 2, 3].map((item) => (
+            <Card key={item} tone={tone} padding={2} radius={2} border>
+              <Flex align="center">
+                <Grid gap={2} columns={2} marginRight={3}>
+                  <Skeleton style={{width: 40, height: 40}} radius={2} animated={animated} />
+                  <Skeleton style={{width: 40, height: 40}} radius={2} animated={animated} />
+                  <Skeleton style={{width: 40, height: 40}} radius={2} animated={animated} />
+                  <Skeleton style={{width: 40, height: 40}} radius={2} animated={animated} />
+                </Grid>
+                <Stack space={2} flex={1}>
+                  <HeadingSkeleton style={{width: '100%'}} radius={1} animated={animated} />
+                  <TextSkeleton style={{width: '100%'}} radius={1} animated={animated} />
+                  <LabelSkeleton style={{width: '100%'}} radius={1} animated={animated} />
+                  <CodeSkeleton style={{width: '100%'}} radius={1} animated={animated} />
+                </Stack>
+              </Flex>
+              <Flex marginTop={2}>
+                <Skeleton
+                  style={{height: 50}}
+                  flex={1}
+                  marginRight={1}
+                  radius={2}
+                  animated={animated}
+                />
+                <Skeleton
+                  style={{height: 50}}
+                  flex={1}
+                  marginLeft={1}
+                  radius={2}
+                  animated={animated}
+                />
+              </Flex>
+            </Card>
+          ))}
+        </Stack>
+      </Container>
+    </Box>
+  )
+}
 
-        <Card marginY={4} radius={2} shadow={1}>
-          <Flex align="center" padding={3} gap={3}>
-            <Skeleton style={{height: 35, width: 35}} />
-            <Stack flex={1} space={2}>
-              <Skeleton style={{height: 11, width: '50%'}} />
-              <Skeleton style={{height: 9, width: '60%'}} />
+function SkeletonDelayStory() {
+  const tone = useSelect('Tone', TONE_OPTIONS, '', 'Props') || 'default'
+  const animated = useBoolean('Animated', true)
+
+  return (
+    <Box padding={[4, 5, 6]}>
+      <Container width={1}>
+        <Text muted>Delayed by 2000ms</Text>
+        <Card tone={tone} marginTop={4} padding={2} radius={2} border>
+          <Flex align="center">
+            <Skeleton
+              style={{width: 90, height: 90}}
+              radius={2}
+              animated={animated}
+              marginRight={3}
+              delay={1000}
+            />
+            <Stack space={2} flex={1}>
+              <HeadingSkeleton
+                size={4}
+                style={{width: '100%'}}
+                radius={1}
+                animated={animated}
+                delay={1000}
+              />
+              <TextSkeleton
+                size={1}
+                style={{width: '100%'}}
+                radius={1}
+                animated={animated}
+                delay={1000}
+              />
+              <LabelSkeleton
+                size={1}
+                style={{width: '100%'}}
+                radius={1}
+                animated={animated}
+                delay={1000}
+              />
+              <CodeSkeleton
+                size={1}
+                style={{width: '100%'}}
+                radius={1}
+                animated={animated}
+                delay={1000}
+              />
             </Stack>
           </Flex>
         </Card>
