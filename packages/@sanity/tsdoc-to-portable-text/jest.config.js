bbd0aa22596729e4878a38285286f19fde89a308
Marius Lundgård <studio@mariuslundgard.com>
2021-06-14 20:39:19 +0200

    chore(tsdoc-to-portable-text): increase test timeout

--- packages/@sanity/tsdoc-to-portable-text/jest.config.js
+++ packages/@sanity/tsdoc-to-portable-text/jest.config.js
@@ -1,10 +1,11 @@
 'use strict'
 
 module.exports = {
+  modulePathIgnorePatterns: ['<rootDir>/test/__fixtures'],
   testEnvironment: 'node',
+  testTimeout: 10000,
   transform: {
     '^.+\\.tsx?$': 'esbuild-jest',
   },
   verbose: true,
-  modulePathIgnorePatterns: ['<rootDir>/test/__fixtures'],
 }
