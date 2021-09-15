00951d19aefa4ced871b96fa02d92f8047b431a5
Marius Lundgård <studio@mariuslundgard.com>
2021-09-15 09:19:08 +0200

    refactor(workshop): improve location provider

--- apps/workshop/src/main.tsx
+++ apps/workshop/src/main.tsx
@@ -48,19 +48,19 @@ const GlobalStyle = createGlobalStyle`
 `
 
 function Root() {
-  const {path, pushState, replaceState} = useLocation()
+  const {path, pushState, query, replaceState} = useLocation()
 
   const handleLocationPush = useCallback(
-    (newLoc: WorkshopLocation) => pushState({path: newLoc.path}),
+    (newLoc: WorkshopLocation) => pushState(newLoc),
     [pushState]
   )
 
   const handleLocationReplace = useCallback(
-    (newLoc: WorkshopLocation) => replaceState({path: newLoc.path}),
+    (newLoc: WorkshopLocation) => replaceState(newLoc),
     [replaceState]
   )
 
-  const studioLocation: WorkshopLocation = useMemo(() => ({path}), [path])
+  const studioLocation: WorkshopLocation = useMemo(() => ({path, query}), [path, query])
 
   const prefersDark = usePrefersDark()
   const [scheme, setScheme] = useState<'light' | 'dark'>(prefersDark ? 'dark' : 'light')
