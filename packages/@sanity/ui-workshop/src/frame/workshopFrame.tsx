669a8503030a1d0543f3d284dfa8a1ef6492e4f9
Marius Lundgård <studio@mariuslundgard.com>
2021-09-21 17:55:27 +0200

    feat(ui-workshop): add error boundary

--- packages/@sanity/ui-workshop/src/frame/workshopFrame.tsx
+++ packages/@sanity/ui-workshop/src/frame/workshopFrame.tsx
@@ -1,7 +1,14 @@
+import {UndoIcon} from '@sanity/icons'
 import {
   BoundaryElementProvider,
+  Button,
   Card,
+  Code,
+  Container,
+  ErrorBoundary,
   PortalProvider,
+  Stack,
+  Text,
   ThemeColorSchemeKey,
   ToastProvider,
 } from '@sanity/ui'
@@ -181,29 +188,76 @@ const InnerWorkshopFrame = forwardRef(function InnerWorkshopFrame(
   // Set mounted
   useEffect(() => setMounted(true), [])
 
+  const [errorParams, setErrorParams] = useState<{
+    error: Error
+    info: React.ErrorInfo
+  } | null>(null)
+
+  const {error, info: errorInfo} = errorParams || {}
+
+  const handleReset = useCallback(() => setErrorParams(null), [])
+
+  if (error) {
+    return (
+      <Container paddingX={4} paddingY={[5, 5, 6]} sizing="border">
+        <Stack height="fill" space={5}>
+          <Text as="h1" weight="bold">
+            Workshop error
+          </Text>
+
+          <Stack>
+            <Button icon={UndoIcon} onClick={handleReset} text="Retry" />
+          </Stack>
+
+          <Stack space={3}>
+            <Text as="h2" size={1} weight="semibold">
+              Error stack
+            </Text>
+            <Card border overflow="auto" padding={3} radius={2} tone="critical">
+              <Code>{error.stack}</Code>
+            </Card>
+          </Stack>
+
+          {errorInfo?.componentStack && (
+            <Stack space={3}>
+              <Text as="h2" size={1} weight="semibold">
+                Component stack
+              </Text>
+              <Card border overflow="auto" padding={3} radius={2} tone="critical">
+                <Code>{'    ' + errorInfo.componentStack.trim()}</Code>
+              </Card>
+            </Stack>
+          )}
+        </Stack>
+      </Container>
+    )
+  }
+
   if (!mounted) {
     return <></>
   }
 
   return (
-    <WorkshopContext.Provider value={contextValue}>
-      <ScopeProvider
-        axeResults={state.axeResults}
-        schemas={state.schemas}
-        registerProp={registerProp}
-        scope={scope}
-        setPropValue={setPropValue}
-        story={story}
-        title={title}
-        unregisterProp={unregisterProp}
-        value={state.value}
-      >
-        <Suspense fallback={null}>
-          <Card as="main" height="fill" ref={ref}>
-            {story && createElement(story.component)}
-          </Card>
-        </Suspense>
-      </ScopeProvider>
-    </WorkshopContext.Provider>
+    <ErrorBoundary onCatch={setErrorParams}>
+      <WorkshopContext.Provider value={contextValue}>
+        <ScopeProvider
+          axeResults={state.axeResults}
+          schemas={state.schemas}
+          registerProp={registerProp}
+          scope={scope}
+          setPropValue={setPropValue}
+          story={story}
+          title={title}
+          unregisterProp={unregisterProp}
+          value={state.value}
+        >
+          <Suspense fallback={null}>
+            <Card as="main" height="fill" ref={ref}>
+              {story && createElement(story.component)}
+            </Card>
+          </Suspense>
+        </ScopeProvider>
+      </WorkshopContext.Provider>
+    </ErrorBoundary>
   )
 })
