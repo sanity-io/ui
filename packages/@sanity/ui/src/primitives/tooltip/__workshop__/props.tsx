53afa53af19f8a1059b3cfadf57dc627cd977e29
Marius Lundgård <studio@mariuslundgard.com>
2021-09-23 19:23:14 +0200

    test(ui): improve tooltip workshop

--- packages/@sanity/ui/src/primitives/tooltip/__workshop__/props.tsx
+++ packages/@sanity/ui/src/primitives/tooltip/__workshop__/props.tsx
@@ -1,11 +1,9 @@
-import {Box, Button, Card, Flex, Text, Tooltip, useTheme} from '@sanity/ui'
+import {Box, Button, Card, Flex, Text, Tooltip} from '@sanity/ui'
 import React from 'react'
 
 export default function PropsStory() {
-  const {dark} = useTheme().sanity.color
-
   return (
-    <Card height="fill" scheme={dark ? 'light' : 'dark'}>
+    <Card height="fill">
       <Flex align="center" height="fill" justify="center" padding={4} sizing="border">
         <Tooltip
           content={
@@ -15,7 +13,6 @@ export default function PropsStory() {
           }
           placement="top"
           portal
-          scheme={dark ? 'dark' : 'light'}
         >
           <Button mode="bleed" text="Hover me" />
         </Tooltip>
