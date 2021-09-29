55555e4151d5f88ea895d80fb0105d95e93046d1
Marius Lundgård <studio@mariuslundgard.com>
2021-09-29 18:37:34 +0200

    fix(ui): focus the first focusable element in dialogs

--- src/components/dialog/__workshop__/autoFocus.tsx
+++ src/components/dialog/__workshop__/autoFocus.tsx
@@ -1,10 +1,11 @@
 import {Box, Button, Dialog, Text} from '@sanity/ui'
-import {useBoolean} from '@sanity/ui-workshop'
+import {useAction, useBoolean} from '@sanity/ui-workshop'
 import React from 'react'
 
 export default function AutoFocusStory() {
   const autoFocus = useBoolean('Auto-focus', true, 'Props')
   const open = useBoolean('Open', false, 'Props')
+  const handleClose = useAction('onClose')
 
   if (!open) {
     return (
@@ -15,7 +16,12 @@ export default function AutoFocusStory() {
   }
 
   return (
-    <Dialog __unstable_autoFocus={autoFocus} header="Auto-focus example" id="auto-focus-example">
+    <Dialog
+      __unstable_autoFocus={autoFocus}
+      header="Auto-focus example"
+      id="auto-focus-example"
+      onClose={handleClose}
+    >
       <Box padding={4}>
         <Button text="Focusable button" />
       </Box>
