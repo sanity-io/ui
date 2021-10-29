9ed1d9b973868c14e10c0371b993aa3f9a4d8247
Marius Lundgård <studio@mariuslundgard.com>
2021-10-29 05:56:56 +0200

    refactor(docs): hide 404 image when dark

--- apps/docs/screens/notFound/notFound.tsx
+++ apps/docs/screens/notFound/notFound.tsx
@@ -1,13 +1,23 @@
-import {Box, Container, Flex, Heading} from '@sanity/ui'
+import {Box, Container, Flex, Heading, useTheme} from '@sanity/ui'
 import Image from 'next/image'
 import React from 'react'
 import {basePath} from '$config'
 
 export function NotFoundScreen() {
+  const {color} = useTheme().sanity
+
   return (
     <Flex align="center" height="fill" justify="center">
       <Container padding={4} width={1}>
-        <Image aria-hidden src={`${basePath}/images/judges-404@2x.png`} width="1253" height="681" />
+        {!color.dark && (
+          <Image
+            aria-hidden
+            src={`${basePath}/images/judges-404@2x.png`}
+            width="1253"
+            height="681"
+          />
+        )}
+
         <Box marginTop={[3, 4, 5]}>
           <Heading align="center" as="h1" size={[2, 2, 3, 4]}>
             Page not found
