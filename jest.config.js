60d228414f160629593dec3977e9c9b3b4b62706
Marius Lundgård <studio@mariuslundgard.com>
2021-11-26 17:10:00 +0100

    build: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
+'use strict'
+
 module.exports = {
-  transform: {'^.+\\.tsx?$': 'esbuild-jest'},
+  modulePathIgnorePatterns: ['<rootDir>/lib/'],
+  setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
   testEnvironment: 'jsdom',
   // - match all files in `__tests__` directories
-  // - match files ending with `.test.js`, `.test.ts`, `.test.jsx`, or `.test.tsx
+  // - match files ending with `.test.js`, `.test.ts`, `.test.jsx`, or `.test.tsx`
   testRegex: '(/__tests__/.*|\\.test)\\.[jt]sx?$',
-  setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
-  modulePathIgnorePatterns: ['<rootDir>/lib/'],
+  transform: {'^.+\\.tsx?$': 'esbuild-jest'},
 }
