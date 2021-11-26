64be892095f46231b72e1ad2d084dad1dd385e31
Marius Lundgård <studio@mariuslundgard.com>
2021-11-27 00:44:09 +0100

    build(studio): alias design libs

---
+++ apps/studio/webpack.sanity.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+'use strict'
+
+const path = require('path')
+
+const ROOT_PATH = path.resolve(__dirname, '../..')
+
+module.exports = (baseConfig) => ({
+  ...baseConfig,
+  resolve: {
+    ...baseConfig?.resolve,
+    alias: {
+      ...baseConfig?.resolve?.alias,
+      '@sanity/color': path.resolve(ROOT_PATH, 'packages/@sanity/color/src'),
+      '@sanity/icons': path.resolve(ROOT_PATH, 'packages/@sanity/icons/src'),
+      '@sanity/logos': path.resolve(ROOT_PATH, 'packages/@sanity/logos/src'),
+      '@sanity/ui': path.resolve(ROOT_PATH, 'packages/@sanity/ui/src'),
+    },
+  },
+})
