b353fe0c725b22ce09a5d7eaeb23996a8da241fd
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-01-18 19:07:04 -0500

    refactor(ui): use simpler isEqual for useUnique hook

--- packages/@sanity/ui/src/hooks/_internal/useUnique.ts
+++ packages/@sanity/ui/src/hooks/_internal/useUnique.ts
@@ -1,13 +1,15 @@
-import {isEqual} from 'lodash'
 import {useRef} from 'react'
 
 /**
- * This is React hook – an and escape hatch – to make sure that a value is the same
- * on every render. SHOULD NOT BE USED IN MOST CASES.
+ * This is a React hook – and an escape hatch – to make sure that a record is
+ * the same on every render. Uses strict equality comparison (eg by identity),
+ * and only goes one level deep. SHOULD NOT BE USED IN MOST CASES.
  *
  * @internal
  */
-export function useUnique<ValueType>(value: ValueType): ValueType {
+type Comparable = Record<string | number | symbol, unknown> | undefined | null
+
+export function useUnique<ValueType extends Comparable = Comparable>(value: ValueType): ValueType {
   const valueRef = useRef<ValueType>(value)
 
   if (!isEqual(valueRef.current, value)) {
@@ -15,4 +17,19 @@ export function useUnique<ValueType>(value: ValueType): ValueType {
   }
 
   return valueRef.current
+}
+
+function isEqual(objA: Comparable, objB: Comparable): boolean {
+  if (!objA || !objB) {
+    return objA === objB
+  }
+
+  const keysA = Object.keys(objA)
+  const keysB = Object.keys(objB)
+
+  if (keysA.length !== keysB.length) {
+    return false
+  }
+
+  return keysA.every((key) => objA[key] === objB[key])
 }
