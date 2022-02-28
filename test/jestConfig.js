a417f014c5debe5eeca932f477d5ade044160ebd
Marius Lundgård <studio@mariuslundgard.com>
2022-02-28 14:11:04 +0100

    test: use sourcemaps for tests (esbuild)

--- test/jestConfig.js
+++ test/jestConfig.js
@@ -26,6 +26,6 @@ exports.createJestConfig = (
     // - match all files in `__tests__` directories
     // - match files ending with `.test.js`, `.test.ts`, `.test.jsx`, or `.test.tsx`
     testRegex: '(/__tests__/.*|\\.test)\\.[jt]sx?$',
-    transform: {'^.+\\.tsx?$': 'esbuild-jest'},
+    transform: {'^.+\\.tsx?$': ['esbuild-jest', {sourcemap: true}]},
   }
 }
