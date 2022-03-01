e3c5aa40a35c9afd8f6b3d9b2417f77a9299713a
Marius Lundgård <studio@mariuslundgard.com>
2022-03-01 14:27:08 +0100

    test(ui): update workshop

--- packages/@sanity/ui/src/components/tree/__workshop__/index.ts
+++ packages/@sanity/ui/src/components/tree/__workshop__/index.ts
@@ -2,5 +2,10 @@ import {defineScope} from '@sanity/ui-workshop'
 import {lazy} from 'react'
 
 export default defineScope('components/tree', 'Tree', [
-  {name: 'basic', title: 'Basic', component: lazy(() => import('./basic'))},
+  {
+    name: 'basic',
+    title: 'Basic',
+    component: lazy(() => import('./basic')),
+    // options: {perfTests: () => import('./basic.perf')},
+  },
 ])
