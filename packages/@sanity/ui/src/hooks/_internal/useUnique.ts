6106e9d6b6d15207b6888ad244a335137001226d
Marius Lundgård <studio@mariuslundgard.com>
2022-08-17 00:03:11 +0200

    refactor(ui): add `_` prefix to internal members

--- packages/@sanity/ui/src/hooks/_internal/useUnique.ts
+++ packages/@sanity/ui/src/hooks/_internal/useUnique.ts
@@ -1,9 +1,8 @@
 import {useRef} from 'react'
 
 /**
- * This is a React hook – and an escape hatch – to make sure that a record is
- * the same on every render. Uses strict equality comparison (eg by identity),
- * and only goes one level deep. SHOULD NOT BE USED IN MOST CASES.
+ * This is a React hook to make sure that a record identity is the same on every render. Uses strict
+ * equality comparison (eg by identity), and only goes one level deep.
  *
  * @internal
  */
@@ -12,14 +11,14 @@ type Comparable = Record<string | number | symbol, unknown> | undefined | null
 export function useUnique<ValueType extends Comparable = Comparable>(value: ValueType): ValueType {
   const valueRef = useRef<ValueType>(value)
 
-  if (!isEqual(valueRef.current, value)) {
+  if (!_isEqual(valueRef.current, value)) {
     valueRef.current = value
   }
 
   return valueRef.current
 }
 
-function isEqual(objA: Comparable, objB: Comparable): boolean {
+function _isEqual(objA: Comparable, objB: Comparable): boolean {
   if (!objA || !objB) {
     return objA === objB
   }
