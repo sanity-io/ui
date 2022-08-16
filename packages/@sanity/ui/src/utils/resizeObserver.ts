c76ecd6c37ad7fa7c4d312a6c6c1e42a6bfc3ff3
Marius Lundgård <studio@mariuslundgard.com>
2022-08-16 16:01:41 +0200

    refactor(ui): improve `ResizeObserver` typing

--- packages/@sanity/ui/src/utils/resizeObserver.ts
+++ packages/@sanity/ui/src/utils/resizeObserver.ts
@@ -3,10 +3,9 @@ import {ResizeObserver as ResizeObserverPolyfill} from '@juggle/resize-observer'
 /**
  * @internal
  */
-const RO: typeof ResizeObserver = (
+const RO: typeof ResizeObserver =
   typeof window !== 'undefined' && window.ResizeObserver
     ? window.ResizeObserver
     : ResizeObserverPolyfill
-) as any
 
 export {RO as ResizeObserver}
