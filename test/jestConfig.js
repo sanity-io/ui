e7b282f0b819b53d3a24de57a24f254faebd82c3
Marius Lundgård <studio@mariuslundgard.com>
2022-09-07 17:22:34 +0200

    test: configure esbuild-jest

--- test/jestConfig.js
+++ test/jestConfig.js
@@ -26,6 +26,16 @@ exports.createJestConfig = (
     // - match all files in `__tests__` directories
     // - match files ending with `.test.js`, `.test.ts`, `.test.jsx`, or `.test.tsx`
     testRegex: '(/__tests__/.*|\\.test)\\.[jt]sx?$',
-    transform: {'^.+\\.tsx?$': ['esbuild-jest', {sourcemap: true}]},
+    transform: {
+      '^.+\\.tsx?$': [
+        'esbuild-jest',
+        {
+          sourcemap: true,
+          loaders: {
+            '.test.tsx': 'tsx',
+          },
+        },
+      ],
+    },
   }
 }
