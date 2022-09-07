e52e0256a6a5ad882a57df1fa4efa6023d9718fc
Cody Olsen <stipsan@gmail.com>
2022-09-07 16:00:52 +0200

    docs: fix papercuts (#989)

--- apps/docs/sanity.ts
+++ apps/docs/sanity.ts
@@ -3,5 +3,9 @@ import {createCurrentUserHook, createPreviewSubscriptionHook} from 'next-sanity'
 import {sanity} from './config'
 
 export const imageUrlBuilder = createImageUrlBuilder(sanity)
-export const usePreviewSubscription: any = createPreviewSubscriptionHook(sanity)
+export const usePreviewSubscription: any = createPreviewSubscriptionHook({
+  ...sanity,
+  // No limit, to test the perf in extreme cases
+  documentLimit: Infinity,
+})
 export const useCurrentUser = createCurrentUserHook(sanity)
