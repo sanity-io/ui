9925dbcadf524f9b69d7f5bc8028dbeeb03dd8a5
Marius Lundgård <studio@mariuslundgard.com>
2022-09-07 17:43:08 +0200

    fix(docs): fix ts errors

--- apps/docs/sanity.server.ts
+++ apps/docs/sanity.server.ts
@@ -1,14 +1,14 @@
-import {createClient} from 'next-sanity'
+import {createClient, SanityClient} from 'next-sanity'
 import {sanity} from '$config'
 
-export const sanityClient = createClient(sanity)
-export const previewClient = createClient({
+export const sanityClient: SanityClient = createClient(sanity)
+export const previewClient: SanityClient = createClient({
   ...sanity,
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
 })
 
-export const getClient = (preview?: boolean) => {
+export const getClient = (preview?: boolean): SanityClient => {
   if (preview) {
     return previewClient
   }
