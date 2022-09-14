915b0384564b09a8b3ac4274c76e064ac0f14ab4
Marius Lundgård <studio@mariuslundgard.com>
2022-09-15 00:19:42 +0200

    test: revert to `jest`

---
+++ packages/logos/jest.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+'use strict'
+
+const {createJestConfig} = require('../../../test/jestConfig')
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = createJestConfig({
+  displayName: require('./package.json').name,
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+})
