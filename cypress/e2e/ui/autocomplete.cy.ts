e4cc411c11cd3b932c9d77d467c147dc58f39832
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-09-27 13:57:40 +0200

    chore(cypress): add longer timeout

--- cypress/e2e/ui/autocomplete.cy.ts
+++ cypress/e2e/ui/autocomplete.cy.ts
@@ -38,7 +38,7 @@ context('Components/Autocomplete', () => {
     cy.get('#custom-listbox').realPress('ArrowDown')
 
     // Enter to select
-    cy.get('[data-qa="option-NO"]', {timeout: 10000}).should('have.focus').realPress('{enter}')
+    cy.get('[data-qa="option-NO"]').should('have.focus').realPress('{enter}')
 
     // Tab 1 time
     cy.get('#custom').should('have.focus').realPress('Tab')
