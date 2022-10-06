41911505db4a24be2bdeaf4135e492a20478146f
Marius Lundgård <studio@mariuslundgard.com>
2022-10-07 01:47:22 +0200

    test: search for project directories

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+const path = require('path')
+const globby = require('globby')
 const {workspaces} = require('./package.json')
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  projects: workspaces.map((pattern) => `<rootDir>/${pattern}`),
+  projects: globby
+    .sync(workspaces.map((p) => path.join(__dirname, p, 'jest.config.js')))
+    .map((p) => `<rootDir>/${path.relative(__dirname, path.dirname(p))}`),
 }
