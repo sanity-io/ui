82251e05bcf656dba274bf5b37811a82dcb23fcf
Marius Lundgård <studio@mariuslundgard.com>
2022-10-07 11:55:40 +0200

    test(ui): update workshop

--- packages/@sanity/ui/src/primitives/tooltip/__workshop__/props.tsx
+++ packages/@sanity/ui/src/primitives/tooltip/__workshop__/props.tsx
@@ -1,23 +1,26 @@
-import {Box, Button, Card, Flex, Text, Tooltip} from '@sanity/ui'
-import {useSelect, useText} from '@sanity/ui-workshop'
-import {WORKSHOP_PLACEMENT_OPTIONS, WORKSHOP_SHADOW_OPTIONS} from '../../../__workshop__/constants'
+import {Button, Card, Flex, Text, Tooltip} from '@sanity/ui'
+import {useBoolean, useSelect, useText} from '@sanity/ui-workshop'
+import {
+  WORKSHOP_PLACEMENT_OPTIONS,
+  WORKSHOP_SHADOW_OPTIONS,
+  WORKSHOP_SPACE_OPTIONS,
+} from '../../../__workshop__/constants'
 
 export default function PropsStory() {
   const content = useText('Content', 'Tooltip content')
+  const padding = useSelect('Padding', WORKSHOP_SPACE_OPTIONS, 2, 'Props')
   const placement = useSelect('Placement', WORKSHOP_PLACEMENT_OPTIONS, 'top')
+  const portal = useBoolean('Portal', true)
   const shadow = useSelect('Shadow', WORKSHOP_SHADOW_OPTIONS, 2)
 
   return (
     <Card height="fill">
       <Flex align="center" height="fill" justify="center" padding={4} sizing="border">
         <Tooltip
-          content={
-            <Box padding={2}>
-              <Text size={1}>{content}</Text>
-            </Box>
-          }
+          content={<Text size={1}>{content}</Text>}
+          padding={padding}
           placement={placement}
-          portal
+          portal={portal}
           shadow={shadow}
         >
           <Button mode="bleed" text="Hover me" />
