dc2d5edccec2ac27f362f14447b40ea5b187bd00
Marius Lundgård <studio@mariuslundgard.com>
2022-10-31 09:29:20 +0100

    test(ui): update tests after optimization

--- packages/@sanity/ui/src/styles/margin/marginsStyle.test.ts
+++ packages/@sanity/ui/src/styles/margin/marginsStyle.test.ts
@@ -10,24 +10,22 @@ describe('styles/margin', () => {
   it('should 1', () => {
     const styles = responsiveMarginStyle({$margin: [0], theme})
 
-    expect(styles).toEqual([[{margin: 0}]])
+    expect(styles).toEqual([{margin: 0}])
   })
 
   it('should 2', () => {
     const styles = responsiveMarginStyle({$margin: [0, 1, 2], theme})
 
     expect(styles).toEqual([
-      [
-        {
-          margin: 0,
-        },
-        {
-          '@media screen and (min-width: 360px)': {margin: '0.25rem'},
-        },
-        {
-          '@media screen and (min-width: 600px)': {margin: '0.5rem'},
-        },
-      ],
+      {
+        margin: 0,
+      },
+      {
+        '@media screen and (min-width: 360px)': {margin: '0.25rem'},
+      },
+      {
+        '@media screen and (min-width: 600px)': {margin: '0.5rem'},
+      },
     ])
   })
 })
