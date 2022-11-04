cdef658835488af8361c1659dd14d5fdd05043bf
Marius Lundgård <studio@mariuslundgard.com>
2022-11-04 12:58:45 +0100

    test: update workshop

--- packages/ui-workshop/src/core/navbar/__workshop__/index.ts
+++ packages/ui-workshop/src/core/navbar/__workshop__/index.ts
@@ -1,15 +1,19 @@
 import {defineScope} from '@sanity/ui-workshop'
 import {lazy} from 'react'
 
-export default defineScope('navbar', 'Navbar', [
-  {
-    name: 'zoom-menu',
-    title: 'Zoom menu',
-    component: lazy(() => import('./ZoomMenuStory')),
-  },
-  {
-    name: 'viewport-menu',
-    title: 'Viewport menu',
-    component: lazy(() => import('./ViewportMenuStory')),
-  },
-])
+export default defineScope({
+  name: 'navbar',
+  title: 'Navbar',
+  stories: [
+    {
+      name: 'zoom-menu',
+      title: 'Zoom menu',
+      component: lazy(() => import('./ZoomMenuStory')),
+    },
+    {
+      name: 'viewport-menu',
+      title: 'Viewport menu',
+      component: lazy(() => import('./ViewportMenuStory')),
+    },
+  ],
+})
