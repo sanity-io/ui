da7576945f1b6bf8996f5e8bde73a72f5a5593c1
Marius Lundgård <studio@mariuslundgard.com>
2022-11-24 12:38:19 +0100

    chore: alias for ui-workshop

--- packages/icons/workshop.config.ts
+++ packages/icons/workshop.config.ts
@@ -1,5 +1,8 @@
 import {defineConfig} from '@sanity/ui-workshop'
 
 export default defineConfig({
+  alias: {
+    '@sanity/icons': typeof __dirname !== 'undefined' ? __dirname + '/src' : '',
+  },
   title: '@sanity/icons',
 })
