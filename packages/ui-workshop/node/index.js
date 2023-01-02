af24ff237cf666d3506389779f5868829b725233
Marius Lundgård <studio@mariuslundgard.com>
2023-01-02 17:37:52 +0100

    build: add `node` exports compatibility

---
+++ packages/ui-workshop/node/index.js
@@ -0,0 +1,27 @@
+import cjs from '../dist/index.cjs'
+
+export const DEFAULT_VIEWPORT_VALUE = cjs.DEFAULT_VIEWPORT_VALUE
+export const DEFAULT_ZOOM_VALUE = cjs.DEFAULT_ZOOM_VALUE
+export const EMPTY_ARRAY = cjs.EMPTY_ARRAY
+export const EMPTY_RECORD = cjs.EMPTY_RECORD
+export const VIEWPORT_OPTIONS = cjs.VIEWPORT_OPTIONS
+export const Workshop = cjs.Workshop
+export const WorkshopFrame = cjs.WorkshopFrame
+export const WorkshopProvider = cjs.WorkshopProvider
+export const ZOOM_OPTIONS = cjs.ZOOM_OPTIONS
+export const createLocationStore = cjs.createLocationStore
+export const createPubsub = cjs.createPubsub
+export const defineConfig = cjs.defineConfig
+export const defineScope = cjs.defineScope
+export const mount = cjs.mount
+export const mountFrame = cjs.mountFrame
+export const propsPlugin = cjs.propsPlugin
+export const useAction = cjs.useAction
+export const useBoolean = cjs.useBoolean
+export const useNumber = cjs.useNumber
+export const useProps = cjs.useProps
+export const useSelect = cjs.useSelect
+export const useString = cjs.useString
+export const useText = cjs.useText
+export const useWorkshop = cjs.useWorkshop
+export const workshopReducer = cjs.workshopReducer
---
+++ packages/ui-workshop/node/plugin-perf.js
@@ -0,0 +1,5 @@
+import cjs from '../dist/plugin-perf.cjs'
+
+export const perfPlugin = cjs.perfPlugin
+export const usePerf = cjs.usePerf
+export const usePerfTest = cjs.usePerfTest
