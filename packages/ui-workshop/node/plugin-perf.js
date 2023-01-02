5c2bf055e79b2bdea04fd6c58e1810056258e489
Marius Lundgård <studio@mariuslundgard.com>
2023-01-02 23:59:32 +0100

    fix(build): auto-generate Node ESM wrappers

--- packages/ui-workshop/node/plugin-perf.js
+++
@@ -1,5 +0,0 @@
-import cjs from '../dist/plugin-perf.cjs'
-
-export const perfPlugin = cjs.perfPlugin
-export const usePerf = cjs.usePerf
-export const usePerfTest = cjs.usePerfTest
--- packages/ui-workshop/node/runtime.js
+++
@@ -1,4 +0,0 @@
-import cjs from '../dist/runtime.cjs'
-
-export const build = cjs.build
-export const dev = cjs.dev
