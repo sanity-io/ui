e2cb416516775fbb322555c9d215e4c60c31f7cd
Marius Lundgård <studio@mariuslundgard.com>
2023-01-19 18:29:29 +0100

    fix: correctly count nested layers

--- src/utils/layer/layerProvider.tsx
+++ src/utils/layer/layerProvider.tsx
@@ -16,20 +16,32 @@ export interface LayerProviderProps {
  */
 export function LayerProvider(props: LayerProviderProps): React.ReactElement {
   const {children, zOffset: zOffsetProp = 0} = props
+
+  // Get parent context values
   const parent = useContext(LayerContext)
+  const parentRegisterChild = parent?.registerChild
+  const parentSize = parent?.size
+
+  // Get z-index offset
   const zOffset = useArrayProp(zOffsetProp)
+
+  // Get responsive z-index value
   const maxMediaIndex = zOffset.length - 1
   const mediaIndex = Math.min(useMediaIndex(), maxMediaIndex)
   const zIndex = parent ? parent.zIndex + zOffset[mediaIndex] : zOffset[mediaIndex]
-  const [size, setSize] = useState(0)
+
+  // This is a state value that is used to keep track of the number of child layers
+  // This is only used by the root `LayerProvider`
+  const [rootSize, setRootSize] = useState(0)
+
+  // Calculate the size of the layer (i.e. number of child layers)
+  const size = parentSize !== undefined ? parentSize - 1 : rootSize
 
   const registerChild = useCallback(() => {
-    setSize((v) => v + 1)
+    setRootSize((v) => v + 1)
 
-    return () => setSize((v) => v - 1)
+    return () => setRootSize((v) => v - 1)
   }, [])
-
-  const parentRegisterChild = parent?.registerChild
 
   useEffect(() => {
     if (!parentRegisterChild) return
@@ -38,8 +50,14 @@ export function LayerProvider(props: LayerProviderProps): React.ReactElement {
   }, [parentRegisterChild])
 
   const value: LayerContextValue = useMemo(
-    () => ({version: 0.0, isTopLayer: size === 0, registerChild, size, zIndex}),
-    [size, registerChild, zIndex]
+    () => ({
+      version: 0.0,
+      isTopLayer: size === 0,
+      registerChild: parentRegisterChild || registerChild,
+      size,
+      zIndex,
+    }),
+    [parentRegisterChild, registerChild, size, zIndex]
   )
 
   return <LayerContext.Provider value={value}>{children}</LayerContext.Provider>
