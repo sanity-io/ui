30eae56bbcbdd4d23bd6d96e5984e8ecd714028c
Cody Olsen <81981+stipsan@users.noreply.github.com>
2023-01-24 13:21:58 +0100

    feat: add `usePrefersReducedMotion` hook

---
+++ src/hooks/usePrefersReducedMotion.test.tsx
@@ -0,0 +1,18 @@
+/** @jest-environment node */
+import {renderToString, renderToStaticMarkup} from 'react-dom/server'
+import {usePrefersReducedMotion} from './usePrefersReducedMotion'
+
+function Log() {
+  const reduceMotion = usePrefersReducedMotion()
+
+  return <>prefers-motion: {JSON.stringify(reduceMotion)}</>
+}
+
+describe('usePrefersReducedMotion', () => {
+  it(`SSR to static markup returns false`, () => {
+    expect(renderToStaticMarkup(<Log />)).toBe('prefers-motion: false')
+  })
+  it(`SSR to markup for hydration doesn't throw`, () => {
+    expect(renderToString(<Log />)).toMatchInlineSnapshot(`"prefers-motion: <!-- -->false"`)
+  })
+})
