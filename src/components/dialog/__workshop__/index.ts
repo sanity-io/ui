8233259a4199cbcf6b562a077bb19868041f85c8
Herman Wikner <wiknerherman@gmail.com>
2023-01-26 10:58:00 +0100

    test(ui): update dialog workshops

--- src/components/dialog/__workshop__/index.ts
+++ src/components/dialog/__workshop__/index.ts
@@ -12,5 +12,8 @@ export default defineScope({
     {name: 'position', title: 'Position', component: lazy(() => import('./position'))},
     {name: 'provider', title: 'Provider', component: lazy(() => import('./provider'))},
     {name: 'auto-focus', title: 'AutoFocus', component: lazy(() => import('./autoFocus'))},
+    {name: 'panes', title: 'Panes', component: lazy(() => import('./panes'))},
+    {name: 'activate', title: 'Activate', component: lazy(() => import('./activate'))},
+    {name: 'wrapped', title: 'Wrapped', component: lazy(() => import('./wrapped'))},
   ],
 })
