6e84577f7e1b6eb073b3806cb87f66481f9bf1c2
Cody Olsen <stipsan@gmail.com>
2023-03-31 17:59:30 +0200

    fix: add `typeof window` guard to ResizeObserver

--- src/observers/resizeObserver.ts
+++ src/observers/resizeObserver.ts
@@ -4,6 +4,6 @@ import {ResizeObserver as ResizeObserverPolyfill} from '@juggle/resize-observer'
  * @internal
  */
 export const _ResizeObserver: typeof ResizeObserver =
-  typeof document !== 'undefined' && window.ResizeObserver
+  typeof document !== 'undefined' && typeof window !== 'undefined' && window.ResizeObserver
     ? window.ResizeObserver
     : ResizeObserverPolyfill
