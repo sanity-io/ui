75c8f0444410a256c221a948bdb0d4f5b1cda176
Marius Lundgård <studio@mariuslundgard.com>
2023-08-22 13:54:42 +0200

    chore: set up refractor

--- workshop.config.ts
+++ workshop.config.ts
@@ -1,6 +1,17 @@
 import {defineConfig} from '@sanity/ui-workshop'
 import {perfPlugin} from '@sanity/ui-workshop/plugin-perf'
 
+import Refractor from 'react-refractor'
+import javascript from 'refractor/lang/javascript'
+import json from 'refractor/lang/json'
+import jsx from 'refractor/lang/jsx'
+import typescript from 'refractor/lang/typescript'
+
+Refractor.registerLanguage(javascript)
+Refractor.registerLanguage(json)
+Refractor.registerLanguage(jsx)
+Refractor.registerLanguage(typescript)
+
 export default defineConfig({
   alias: {
     '@sanity/ui': typeof __dirname !== 'undefined' ? __dirname + '/src' : '',
