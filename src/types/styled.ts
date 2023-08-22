d08859cda72183996bd2739e1d42a6ab65126f6e
Cody Olsen <stipsan@gmail.com>
2023-08-23 01:02:35 +0200

    chore: add missing file

---
+++ src/types/styled.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// Workaround types that are missing in v6
+// https://github.com/styled-components/styled-components/issues/4062
+
+import type {StyledObject} from 'styled-components'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type CSSObject = StyledObject<any>
