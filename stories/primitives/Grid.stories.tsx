37519dd6b445ec99944b8373fe2662ab319d5806
Nina Andal Aarvik <nina@sanity.io>
2023-10-16 12:40:07 +0200

    chore(storybook): add various primitive + component stories (#1163)
    * chore(storybook): add story for breadcrumbs
    
    * feat(tree): add story for tree component
    
    * feat(inline): add story for inline component
    
    * feat(flex): add story for flex component
    
    * feat(grid): add story for grid component
    
    * feat(stack): add story for stack component
    
    * feat(skeleton): add story without tone variations on SkeletonCard
    
    * feat(menu): add story for menu component without menubutton
    
    * feat(stories): clean up stories
    
    * feat(toast): add story for toast
    
    * feat(component): change story for open toast on button
    
    * feat(stories): minor clean ups
    
    * feat(flex): remove unused import
    
    * chore(storybook): remove unnecessary toast story, add height for toast story in docs context

---
+++ stories/primitives/Grid.stories.tsx
@@ -0,0 +1,48 @@
+import type {Meta, StoryObj} from '@storybook/react'
+import {Card, Code, Grid} from '../../src/primitives'
+import {getHeightControls, getOverflowControls, getSpaceControls} from '../controls'
+
+const meta: Meta<typeof Grid> = {
+  component: Grid,
+  args: {
+    children: [
+      Array.from({length: 12}, (_, index) => (
+        <Card key={`grid${index + 1}`} padding={2}>
+          <Code>{index + 1}</Code>
+        </Card>
+      )),
+    ],
+  },
+  argTypes: {
+    padding: getSpaceControls(),
+    paddingBottom: getSpaceControls(),
+    paddingLeft: getSpaceControls(),
+    paddingRight: getSpaceControls(),
+    paddingTop: getSpaceControls(),
+    paddingY: getSpaceControls(),
+    paddingX: getSpaceControls(),
+    margin: getSpaceControls(),
+    marginBottom: getSpaceControls(),
+    marginLeft: getSpaceControls(),
+    marginRight: getSpaceControls(),
+    marginTop: getSpaceControls(),
+    marginY: getSpaceControls(),
+    marginX: getSpaceControls(),
+    height: getHeightControls(),
+    overflow: getOverflowControls(),
+    gap: getSpaceControls(),
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof Grid>
+
+export const Default: Story = {
+  render: (props) => <Grid {...props} />,
+}
+
+export const Columns: Story = {
+  args: {columns: [1, 2, 3, 4, 5, 6, 7]},
+  render: (props) => <Grid {...props} />,
+}
