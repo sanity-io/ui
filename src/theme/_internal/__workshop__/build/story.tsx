00d66f4ed12e861319f4b05fdeb6c715404255d8
Marius Lundgård <studio@mariuslundgard.com>
2023-10-31 22:16:28 +0100

    test(theme): update workshop

--- src/theme/_internal/__workshop__/build/story.tsx
+++ src/theme/_internal/__workshop__/build/story.tsx
@@ -28,7 +28,7 @@ const Root = styled.div`
 
   pre {
     font: inherit;
-    font-family: 'SF Mono';
+    font-family: 'SF Mono', Menlo, monospace;
     margin: 0;
     padding: 0;
   }
@@ -36,30 +36,43 @@ const Root = styled.div`
   .button {
     background-color: var(--button-enabled-bg);
     color: var(--button-enabled-fg);
+    box-shadow:
+      inset 0 0 0 1px var(--border-color),
+      0 1px 0 0 color-mix(in srgb, var(--border-color), transparent 75%);
     text-align: center;
     border-radius: 3px;
     font-weight: 500;
     cursor: default;
+    transition: box-shadow 80ms ease-in-out;
+
+    --border-color: var(--button-enabled-border);
   }
 
   .button:hover {
     background-color: var(--button-hovered-bg);
     color: var(--button-hovered-fg);
+    --border-color: var(--button-hovered-border);
   }
 
   .button:active {
     background-color: var(--button-pressed-bg);
     color: var(--button-pressed-fg);
+    --border-color: var(--button-pressed-border);
+    box-shadow: inset 0 0 0 1px var(--border-color);
   }
 
   .button.selected {
     background-color: var(--button-selected-bg);
     color: var(--button-selected-fg);
+    --border-color: var(--button-selected-border);
+    box-shadow: inset 0 0 0 1px var(--border-color);
   }
 
   .button.disabled {
     background-color: var(--button-disabled-bg);
     color: var(--button-disabled-fg);
+    --border-color: var(--button-disabled-border);
+    box-shadow: inset 0 0 0 1px var(--border-color);
   }
 `
 
@@ -123,18 +136,23 @@ function BaseTonePreview(props: {
 
                         '--button-enabled-bg': button.enabled.bg,
                         '--button-enabled-fg': button.enabled.fg,
+                        '--button-enabled-border': button.enabled.border,
 
                         '--button-hovered-bg': button.hovered.bg,
                         '--button-hovered-fg': button.hovered.fg,
+                        '--button-hovered-border': button.hovered.border,
 
                         '--button-pressed-bg': button.pressed.bg,
                         '--button-pressed-fg': button.pressed.fg,
+                        '--button-pressed-border': button.pressed.border,
 
                         '--button-selected-bg': button.selected.bg,
                         '--button-selected-fg': button.selected.fg,
+                        '--button-selected-border': button.selected.border,
 
                         '--button-disabled-bg': button.disabled.bg,
                         '--button-disabled-fg': button.disabled.fg,
+                        '--button-disabled-border': button.disabled.border,
                       } as any
                     }
                   >
