c31ee3a86f595e57e0ccbd315ead9c1e9a796073
Marius Lundgård <studio@mariuslundgard.com>
2023-11-01 13:41:06 +0100

    refactor(theme): improve readability

--- src/theme/_internal/build/buildTheme.ts
+++ src/theme/_internal/build/buildTheme.ts
@@ -2,24 +2,15 @@ import {ThemeConfig} from '../config'
 import {TMP_Theme} from '../types'
 import {buildColorTheme} from './colorTheme/buildColorTheme'
 import {renderColorTheme} from './colorTheme/renderColorTheme'
-import {resolveColorTokens} from './colorTheme/resolveColorTokens'
-import {defaultColorPalette} from './defaults/colorPalette'
-import {defaultColorTokens} from './defaults/colorTokens'
 
 /** @internal */
 export function buildTheme(config?: ThemeConfig): TMP_Theme {
-  const colorTokens = resolveColorTokens(config?.color?.tokens ?? defaultColorTokens)
+  const color: TMP_Theme['color'] = {
+    light: buildColorTheme({scheme: 'light'}, config),
+    dark: buildColorTheme({scheme: 'dark'}, config),
+  }
 
   return {
-    color: renderColorTheme(config?.color?.palette ?? defaultColorPalette, {
-      light: buildColorTheme(
-        {scheme: 'light'},
-        {...config, color: {...config?.color, tokens: colorTokens}},
-      ),
-      dark: buildColorTheme(
-        {scheme: 'dark'},
-        {...config, color: {...config?.color, tokens: colorTokens}},
-      ),
-    }),
+    color: renderColorTheme(color, config),
   }
 }
