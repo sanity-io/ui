b26ef81ea18af15d5ce1ecd5c80ff16ff38f44d5
Robin Pyon <robin.pyon@gmail.com>
2023-11-05 20:27:03 +0000

    chore(storybook): add text skeleton stories, columnBuilder helper

---
+++ stories/components/HeadingSkeleton.stories.tsx
@@ -0,0 +1,49 @@
+import type {Meta, StoryObj} from '@storybook/react'
+import {HeadingSkeleton, Skeleton} from '../../src/components'
+import {fonts} from '../../src/theme/studioTheme/fonts'
+import {getFontSizeControls, getSpaceControls} from '../controls'
+import {columnBuilder} from '../helpers/columnBuilder'
+
+const meta: Meta<typeof HeadingSkeleton> = {
+  component: HeadingSkeleton,
+  argTypes: {
+    padding: getSpaceControls(),
+    paddingBottom: getSpaceControls(),
+    paddingLeft: getSpaceControls(),
+    paddingRight: getSpaceControls(),
+    paddingTop: getSpaceControls(),
+    paddingY: getSpaceControls(),
+    paddingX: getSpaceControls(),
+    margin: getSpaceControls(),
+    marginBottom: getSpaceControls(),
+    marginLeft: getSpaceControls(),
+    marginRight: getSpaceControls(),
+    marginTop: getSpaceControls(),
+    marginY: getSpaceControls(),
+    marginX: getSpaceControls(),
+    size: getFontSizeControls('heading'),
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof Skeleton>
+
+export const Default: Story = {
+  render: (props) => <HeadingSkeleton {...props} />,
+}
+
+export const Sizes: Story = {
+  parameters: {controls: {exclude: ['size']}},
+  render: (props) => {
+    return (
+      <>
+        {columnBuilder({
+          gap: 4,
+          renderItem: ({value, index}) => <HeadingSkeleton {...props} key={index} size={value} />,
+          rows: [...Array(fonts['code'].sizes.length).keys()],
+        })}
+      </>
+    )
+  },
+}
---
+++ stories/helpers/columnBuilder.tsx
@@ -0,0 +1,23 @@
+import {ComponentProps, ReactNode} from 'react'
+import {Card, Stack} from '../../src/primitives'
+import {ThemeColorSchemeKey} from '../../src/theme'
+
+interface ColumnBuilderProps<T> {
+  gap?: ComponentProps<typeof Stack>['space']
+  renderItem: ({value, index}: {value: T; index: number}) => ReactNode
+  rows: T[]
+  scheme?: ThemeColorSchemeKey
+}
+
+export const columnBuilder = function <T>({
+  gap = 3,
+  renderItem,
+  rows,
+  scheme,
+}: ColumnBuilderProps<T>): ReactNode {
+  return (
+    <Card border={!!scheme} padding={scheme ? 4 : 0} radius={scheme ? 2 : 0} scheme={scheme}>
+      <Stack space={gap}>{rows.map((value, index) => renderItem({index, value}))}</Stack>
+    </Card>
+  )
+}
