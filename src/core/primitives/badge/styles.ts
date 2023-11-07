ddeb0d8e6a606586ff87ccd746778771a2d8847b
Marius Lundgård <studio@mariuslundgard.com>
2023-11-07 13:12:06 +0100

    fix(badge): set color using variable

--- src/core/primitives/badge/styles.ts
+++ src/core/primitives/badge/styles.ts
@@ -11,7 +11,6 @@ export function badgeStyle(props: BadgeStyleProps & ThemeProps): CSSObject {
     '--card-fg-color': color.enabled.fg,
 
     backgroundColor: color.enabled.bg2,
-    color: color.enabled.fg,
     boxShadow: `inset 0 0 0 1px ${color.enabled.border}`,
     cursor: 'default',
 
