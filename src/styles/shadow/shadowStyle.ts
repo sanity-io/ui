a1fcc9f25f2f80d978c55110abf4ef4daddb121f
Marius Lundgård <studio@mariuslundgard.com>
2023-11-07 12:09:35 +0100

    refactor: borders and shadows

--- src/styles/shadow/shadowStyle.ts
+++ src/styles/shadow/shadowStyle.ts
@@ -9,10 +9,10 @@ function toBoxShadow(shadow: BoxShadow, color: string) {
   return `${shadow.map(rem).join(' ')} ${color}`
 }
 
-function shadowStyle(shadow: ThemeShadow | null): CSSObject {
+function shadowStyle(shadow: ThemeShadow | null, outlineWidth: number = 1): CSSObject {
   if (!shadow) return EMPTY_RECORD
 
-  const outline = `0 0 0 ${rem(1)} var(--card-shadow-outline-color)`
+  const outline = `0 0 0 ${rem(outlineWidth)} var(--card-shadow-outline-color)`
   const umbra = toBoxShadow(shadow.umbra, 'var(--card-shadow-umbra-color)')
   const penumbra = toBoxShadow(shadow.penumbra, 'var(--card-shadow-penumbra-color)')
   const ambient = toBoxShadow(shadow.ambient, 'var(--card-shadow-ambient-color)')
@@ -22,7 +22,9 @@ function shadowStyle(shadow: ThemeShadow | null): CSSObject {
 
 export function responsiveShadowStyle(props: ResponsiveShadowStyleProps & ThemeProps): CSSObject[] {
   const {theme} = props
-  const {media, shadows} = theme.sanity
+  const {card, media, shadows} = theme.sanity
 
-  return _responsive(media, props.$shadow, (shadow) => shadowStyle(shadows[shadow]))
+  return _responsive(media, props.$shadow, (shadow) =>
+    shadowStyle(shadows[shadow], card.shadow.outline),
+  )
 }
