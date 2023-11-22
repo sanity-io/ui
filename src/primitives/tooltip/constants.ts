117fe8571fbd45021ff43c2acb78f40c24fe8728
Pedro Bonamin <46196328+pedrobonamin@users.noreply.github.com>
2023-11-22 17:13:44 +0100

    fix(tooltips): update default fallback placements (#1185)

--- src/primitives/tooltip/constants.ts
+++ src/primitives/tooltip/constants.ts
@@ -3,16 +3,16 @@ import {Placement} from '@floating-ui/react-dom'
 export const DEFAULT_TOOLTIP_DISTANCE = 4
 export const DEFAULT_TOOLTIP_PADDING = 4
 export const DEFAULT_FALLBACK_PLACEMENTS: Record<Placement, Placement[]> = {
-  top: ['bottom', 'left', 'right'],
-  'top-start': ['bottom-start', 'left-start', 'right-start'],
-  'top-end': ['bottom-end', 'left-end', 'right-end'],
-  bottom: ['top', 'left', 'right'],
-  'bottom-start': ['top-start', 'left-start', 'right-start'],
-  'bottom-end': ['top-end', 'left-end', 'right-end'],
-  left: ['right', 'top', 'bottom'],
-  'left-start': ['right-start', 'top-start', 'bottom-start'],
-  'left-end': ['right-end', 'top-end', 'bottom-end'],
-  right: ['left', 'top', 'bottom'],
-  'right-start': ['left-start', 'top-start', 'bottom-start'],
-  'right-end': ['left-end', 'top-end', 'bottom-end'],
+  top: ['top-end', 'top-start', 'bottom', 'left', 'right'],
+  'top-start': ['top', 'top-end', 'bottom-start', 'left-start', 'right-start'],
+  'top-end': ['top', 'top-start', 'bottom-end', 'left-end', 'right-end'],
+  bottom: ['bottom-end', 'bottom-start', 'top', 'left', 'right'],
+  'bottom-start': ['bottom', 'bottom-end', 'top-start', 'left-start', 'right-start'],
+  'bottom-end': ['bottom', 'bottom-start', 'top-end', 'left-end', 'right-end'],
+  left: ['left-end', 'left-start', 'right', 'top', 'bottom'],
+  'left-start': ['left', 'left-end', 'right-start', 'top-start', 'bottom-start'],
+  'left-end': ['left', 'left-start', 'right-end', 'top-end', 'bottom-end'],
+  right: ['right-end', 'right-start', 'left', 'top', 'bottom'],
+  'right-start': ['right', 'right-end', 'left-start', 'top-start', 'bottom-start'],
+  'right-end': ['right', 'right-start', 'left-end', 'top-end', 'bottom-end'],
 }
