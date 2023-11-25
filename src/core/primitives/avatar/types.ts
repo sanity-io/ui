542e4b2fd8ea79ee4f9c5db79a297ab9656752a9
Marius Lundgård <studio@mariuslundgard.com>
2023-11-25 11:04:09 +0100

    refactor(avatar): use new color vars

--- src/core/primitives/avatar/types.ts
+++ src/core/primitives/avatar/types.ts
@@ -1,10 +1,11 @@
+import {ThemeColorSpotKey} from '@sanity/ui/theme'
 import {AvatarSize} from '../../types'
 
 /**
  * @internal
  */
 export interface AvatarRootStyleProps {
-  $color: string
+  $color: ThemeColorSpotKey
 }
 
 /**
