b1082ef6bdcd9c2739cf17f4141e3ec13557ddfa
Marius Lundgård <studio@mariuslundgard.com>
2023-11-26 20:08:06 +0100

    fix(props): improve reducer

--- packages/ui-workshop/src/core/plugins/props/propsReducer.ts
+++ packages/ui-workshop/src/core/plugins/props/propsReducer.ts
@@ -5,6 +5,10 @@ import {PropsState} from './types'
 /** @internal */
 export function propsReducer(state: PropsState, msg: WorkshopMsg | PropsMsg): PropsState {
   if (msg.type === 'workshop/props/setValue') {
+    if (state.value === msg.value) {
+      return state
+    }
+
     return {
       ...state,
       value: msg.value,
@@ -12,6 +16,12 @@ export function propsReducer(state: PropsState, msg: WorkshopMsg | PropsMsg): Pr
   }
 
   if (msg.type === 'workshop/props/registerProp') {
+    const schemaIsRegistered = state.schemas.some((s) => s.name === msg.schema.name)
+
+    if (schemaIsRegistered) {
+      return state
+    }
+
     return {
       ...state,
       schemas: state.schemas.concat([msg.schema]),
@@ -19,6 +29,10 @@ export function propsReducer(state: PropsState, msg: WorkshopMsg | PropsMsg): Pr
   }
 
   if (msg.type === 'workshop/props/setPropValue') {
+    if (state.value[msg.name] === msg.value) {
+      return state
+    }
+
     return {
       ...state,
       value: {...state.value, [msg.name]: msg.value},
