2556a45880af057ac756982494622fd5e3dadda4
Marius Lundgård <studio@mariuslundgard.com>
2023-11-28 17:05:47 +0100

    test(cypress): update test

--- cypress/e2e/ui/textInput.cy.ts
+++ cypress/e2e/ui/textInput.cy.ts
@@ -7,7 +7,7 @@ context('Primitives/TextInput', () => {
     cy.get('#text-input-example + span').should(
       'have.css',
       'boxShadow',
-      'rgb(14, 217, 217) 0px 0px 0px 1px inset, rgb(220, 227, 228) 0px 0px 0px 1px inset',
+      'rgb(56, 139, 255) 0px 0px 0px 1px inset, rgb(220, 227, 228) 0px 0px 0px 1px inset',
     )
   })
 })
