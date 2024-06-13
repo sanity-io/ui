fdbf52f51777f4f2f1284255a12f6df25cbfcfdd
Cody Olsen <stipsan@gmail.com>
2024-06-13 22:26:48 +0200

    feat: add `react-compiler` condition

--- package.config.ts
+++ package.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
@@ -14,4 +15,25 @@ export default defineConfig({
     noImplicitBrowsersList: 'off',
   },
   tsconfig: 'tsconfig.dist.json',
+  reactCompilerOptions: {
+    panicThreshold: 'ALL_ERRORS',
+    logger: {
+      logEvent(filename, event) {
+        if (event.kind === 'CompileError') {
+          console.group(`[${filename}] ${event.kind}`)
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const {reason, description, severity, loc, suggestions} = event.detail as any
+
+          console.error(`[${severity}] ${reason}`)
+          console.log(`${filename}:${loc.start.line}:${loc.start.column} ${description}`)
+
+          for (const suggestion of suggestions) {
+            console.log(suggestion.description)
+          }
+
+          console.groupEnd()
+        }
+      },
+    },
+  },
 })
