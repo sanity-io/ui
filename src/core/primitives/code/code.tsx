9ac9cc344dae90c0c0a2e3994ca028e56ca76f07
Bjørge Næss <bjoerge@gmail.com>
2025-02-21 10:56:40 +0100

    fix!: use stricter type definition for children of <Code>
    BREAKING CHANGE: The Code component no longer support JSX as children.

--- src/core/primitives/code/code.tsx
+++ src/core/primitives/code/code.tsx
@@ -1,4 +1,4 @@
-import {forwardRef, Suspense, lazy} from 'react'
+import {forwardRef, lazy, Suspense} from 'react'
 import {styled} from 'styled-components'
 import {useArrayProp} from '../../hooks'
 import {responsiveCodeFontStyle, ResponsiveFontStyleProps} from '../../styles/internal'
@@ -9,10 +9,22 @@ const LazyRefractor = lazy(() => import('./refractor'))
 /**
  * @public
  */
+export type StringifiableNode =
+  | string
+  | number
+  | bigint
+  | boolean
+  | null
+  | undefined
+  | Iterable<StringifiableNode>
+/**
+ * @public
+ */
 export interface CodeProps {
   as?: React.ElementType | keyof React.JSX.IntrinsicElements
   /** Define the language to use for syntax highlighting. */
   language?: string
+  children: StringifiableNode
   size?: number | number[]
   weight?: string
 }
@@ -23,7 +35,7 @@ const StyledCode = styled.pre<ResponsiveFontStyleProps>(codeBaseStyle, responsiv
  * @public
  */
 export const Code = forwardRef(function Code(
-  props: CodeProps & Omit<React.HTMLProps<HTMLElement>, 'as' | 'size'>,
+  props: CodeProps & Omit<React.HTMLProps<HTMLElement>, 'as' | 'size' | 'children'>,
   ref: React.ForwardedRef<HTMLElement>,
 ) {
   const {children, language, size = 2, weight, ...restProps} = props
@@ -31,9 +43,21 @@ export const Code = forwardRef(function Code(
   return (
     <StyledCode data-ui="Code" {...restProps} $size={useArrayProp(size)} $weight={weight} ref={ref}>
       <Suspense fallback={<code>{children}</code>}>
-        <LazyRefractor language={language} value={children} />
+        <LazyRefractor language={language} value={stringifyChildren(children)} />
       </Suspense>
     </StyledCode>
   )
 })
 Code.displayName = 'ForwardRef(Code)'
+
+function stringifyChildren(children: StringifiableNode): string {
+  if (!children || typeof children === 'boolean') {
+    return ''
+  }
+
+  if (Array.isArray(children)) {
+    return children.map((c) => stringifyChildren(c)).join('')
+  }
+
+  return String(children)
+}
