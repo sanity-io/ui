05394e1c0d70134d2b28e9bba516b9a2ec2043b4
Jordan Lawrence <jordanl17@me.com>
2025-05-08 14:23:20 +0100

    feat: new squiggle logo (#129)
    * feat: new squiggle logo
    
    * refactor: simplify SVG structure in SanityLogo and SanityMonogram components
    
    * refactor: reinstate color support for sanityMonogram
    
    * refactor: colors are used in logo and mongram; deprecation on color for monogram

--- packages/logos/src/__workshop__/SanityMonogramStory.tsx
+++ packages/logos/src/__workshop__/SanityMonogramStory.tsx
@@ -1,11 +1,17 @@
-import {Flex, Heading} from '@sanity/ui'
+import {Flex, Heading, useTheme} from '@sanity/ui'
+import {useBoolean} from '@sanity/ui-workshop'
 import {SanityMonogram} from '../sanityMonogram'
 
 export default function SanityMonogramStory() {
+  const isDefaultScheme = useBoolean('Use default', false)
+  const {dark} = useTheme().sanity.color
+
+  const scheme = isDefaultScheme ? 'default' : dark ? 'dark' : 'light'
+
   return (
     <Flex align="center" height="fill" justify="center" padding={5} sizing="border">
       <Heading size={5}>
-        <SanityMonogram />
+        <SanityMonogram scheme={scheme} />
       </Heading>
     </Flex>
   )
--- packages/logos/src/sanityMonogram.tsx
+++ packages/logos/src/sanityMonogram.tsx
@@ -1,11 +1,21 @@
-import {hues, white} from '@sanity/color'
+import {white} from '@sanity/color'
 import {forwardRef} from 'react'
+import {NEW_SANITY_BLACK, NEW_SANITY_ORANGE} from './colors'
 
 /**
  * @public
  */
+export type SanityMonogramScheme = 'light' | 'dark' | 'default'
+
+/**
+ * @public
+ * @deprecated use {@link SanityMonogramScheme} as the `scheme` prop instead
+ */
 export interface SanityMonogramColor {
   bg1: string
+  /**
+   * `bg2` is unused, use `bg1` instead
+   */
   bg2: string
   fg: string
 }
@@ -13,14 +23,15 @@ export interface SanityMonogramColor {
 /**
  * @public
  */
-export interface SanityMonogramProps {
-  color?: SanityMonogramColor
-}
+export type SanityMonogramProps =
+  | {color: SanityMonogramColor; scheme?: undefined}
+  | {color?: undefined; scheme: SanityMonogramScheme}
+  | {color?: undefined; scheme?: undefined}
 
-const SANITY_MONOGRAM_COLOR: SanityMonogramColor = {
-  bg1: hues.red[500].hex,
-  bg2: hues.red[200].hex,
-  fg: white.hex,
+const SANITY_MONOGRAM_COLOR: {[key in SanityMonogramScheme]: {bg: string; fg: string}} = {
+  light: {bg: white.hex, fg: NEW_SANITY_BLACK},
+  dark: {bg: NEW_SANITY_BLACK, fg: white.hex},
+  default: {bg: NEW_SANITY_ORANGE, fg: NEW_SANITY_BLACK},
 }
 
 /**
@@ -30,31 +41,24 @@ export const SanityMonogram = forwardRef(function SanityMonogram(
   props: SanityMonogramProps & Omit<React.SVGProps<SVGSVGElement>, 'color'>,
   ref: React.Ref<SVGSVGElement>,
 ) {
-  const {color = SANITY_MONOGRAM_COLOR, ...restProps} = props
+  const {scheme = 'default', color, ...restProps} = props
+  const bg = color?.bg1 || SANITY_MONOGRAM_COLOR[scheme].bg
+  const fg = color?.fg || SANITY_MONOGRAM_COLOR[scheme].fg
 
   return (
     <svg
       data-sanity-icon="sanity-monogram"
-      width="1em"
       height="1em"
-      viewBox="0 0 128 128"
+      viewBox="0 0 192 192"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...restProps}
       ref={ref}
     >
-      <rect width="128" height="128" rx="8" fill={color.bg1} />
+      <rect width="192" height="192" fill={bg} />
       <path
-        d="M39.423 33.163C39.423 44.1615 46.3363 50.7056 60.1768 54.1564L74.843 57.4972C87.9418 60.453 95.9186 67.7945 95.9186 79.7554C96.0205 84.9662 94.2961 90.0531 91.0345 94.1635C91.0345 82.2301 84.751 75.7822 69.595 71.9052L55.1948 68.6882C43.6633 66.1035 34.7629 60.0681 34.7629 47.0761C34.7022 42.0589 36.3416 37.1644 39.423 33.163"
-        fill={color.fg}
-      />
-      <path
-        d="M82.0221 76.827C88.2776 80.759 91.0206 86.2582 91.0206 94.1497C85.8426 100.666 76.7462 104.323 66.0545 104.323C48.0576 104.323 35.4626 95.6207 32.6637 80.4977H49.9468C52.172 87.4406 58.0636 90.6576 65.9285 90.6576C75.5287 90.6576 81.9102 85.6258 82.0361 76.7995"
-        fill={color.bg2}
-      />
-      <path
-        d="M48.4075 49.4682C45.551 47.8004 43.2074 45.404 41.6256 42.5332C40.0437 39.6624 39.2826 36.4244 39.4231 33.1629C44.4191 26.7013 53.1096 22.7556 63.7034 22.7556C82.0362 22.7556 92.6439 32.2693 95.2609 45.66H78.6355C76.8022 40.3807 72.2121 36.27 63.8434 36.27C54.9009 36.27 48.7993 41.3843 48.4495 49.4682"
-        fill={color.bg2}
+        d="M160.077 112.697L154.865 103.629L129.659 118.981L157.655 83.3368L161.888 80.8533L160.841 79.2802L162.764 76.8232L153.929 69.4699L149.886 74.6225L68.2657 122.375L98.4429 86.0855L154.651 55.2759L149.311 44.953L118.696 61.7277L133.771 43.6096L125.134 36L91.2055 76.7966L57.5083 95.2771L83.307 61.1709L99.4731 52.757L94.3391 42.3192L47.2403 66.8361L60.0839 49.8405L51.1123 42.6551L24 78.5378L24.4207 78.8736L29.486 89.1877L59.543 73.5354L32.1474 109.745L36.6375 113.342L39.3075 118.504L70.9528 101.154L36.1052 143.065L44.742 150.674L46.4762 148.588L130.543 99.2454L102.632 134.792L103.088 135.172L103.045 135.199L108.831 145.265L145.954 122.649L131.659 145.716L141.24 152L164 115.278L160.077 112.697Z"
+        fill={fg}
       />
     </svg>
   )
