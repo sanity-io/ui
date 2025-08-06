5e14a46e0127ccb6b645f7f883f1f1ea6d9af1d8
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-08-06 15:25:59 +0200

    feat: remove `styled-components` dependency (#181)

--- packages/ui-workshop/src/core/WorkshopCanvas.tsx
+++ packages/ui-workshop/src/core/WorkshopCanvas.tsx
@@ -1,18 +1,18 @@
-import {BoxDisplay, Card, Container, Flex, Heading, Spinner, Stack, Text} from '@sanity/ui'
-import {memo, useMemo, useState} from 'react'
-import {styled} from 'styled-components'
+import {Card, Container, Flex, Heading, Spinner, Stack, Text} from '@sanity/ui'
+import {assignInlineVars} from '@vanilla-extract/dynamic'
+import {memo, useState} from 'react'
+
+import {
+  iframe,
+  iframeContainer,
+  viewportMaxHeight,
+  viewportMaxWidth,
+  zoom as zoomVar,
+} from '#styles'
 
 import {VIEWPORT_OPTIONS} from './constants'
 import {buildFrameUrl} from './helpers'
 import {useWorkshop} from './useWorkshop'
-
-const Frame = styled.iframe`
-  display: block;
-  border: 0;
-  height: 100%;
-  width: 100%;
-  view-transition-name: canvas;
-`
 
 /** @internal */
 export const WorkshopCanvas = memo(function WorkshopCanvas(props: {
@@ -29,28 +29,8 @@ export const WorkshopCanvas = memo(function WorkshopCanvas(props: {
     buildFrameUrl({baseUrl: frameUrl, path, payload, scheme, viewport, zoom}),
   )
 
-  const containerStyle = useMemo(
-    () => ({
-      maxWidth: viewportW === 'auto' ? undefined : `${(viewportW || 1) * zoom}px`,
-      maxHeight: viewportH ? `${(viewportH || 1) * zoom}px` : undefined,
-    }),
-    [viewportW, viewportH, zoom],
-  )
-
-  const display: BoxDisplay = useMemo(() => (hidden ? 'none' : 'block'), [hidden])
-
-  const frameStyle = useMemo(
-    () => ({
-      transform: `scale(${zoom})`,
-      transformOrigin: '0 0',
-      width: `${100 / zoom}%`,
-      height: `${100 / zoom}%`,
-    }),
-    [zoom],
-  )
-
   return (
-    <Card display={display} flex={1} overflow="hidden" tone="transparent">
+    <Card display={hidden ? 'none' : 'block'} flex={1} overflow="hidden" tone="transparent">
       <Flex align="center" height="fill" justify="center" sizing="border">
         {path === '/' && (
           <Container width={0}>
@@ -66,13 +46,18 @@ export const WorkshopCanvas = memo(function WorkshopCanvas(props: {
         {!frameReady && path !== '/' && <Spinner muted />}
 
         <Container
+          className={iframeContainer}
           height="fill"
           hidden={!frameReady || path === '/'}
-          style={containerStyle}
+          style={assignInlineVars({
+            [viewportMaxWidth]: viewportW === 'auto' ? undefined : `${viewportW}px`,
+            [viewportMaxHeight]: viewportH ? `${viewportH}px` : undefined,
+            [zoomVar]: `${zoom}`,
+          })}
           width="auto"
         >
           <Card height="fill" shadow={1}>
-            <Frame ref={frameRef} src={initialFrameUrl} style={frameStyle} />
+            <iframe className={iframe} ref={frameRef} src={initialFrameUrl} />
           </Card>
         </Container>
       </Flex>
