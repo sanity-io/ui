522fc50c8f3a2980db2f3883f641db67739cff1c
Marius Lundgård <studio@mariuslundgard.com>
2025-08-21 12:26:33 +0200

    fix: narrow down `popover` property type

--- src/core/components/autocomplete/autocomplete.tsx
+++ src/core/components/autocomplete/autocomplete.tsx
@@ -63,7 +63,7 @@ export type AutocompleteOwnProps<O extends BaseAutocompleteOption = BaseAutocomp
     /** The options to render. */
     options?: O[]
     padding?: ResponsiveProp<Space>
-    popover?: Omit<Props<PopoverProps, 'div'>, 'content' | 'onMouseEnter' | 'onMouseLeave' | 'open'>
+    popover?: Omit<PopoverProps<'div'>, 'content' | 'onMouseEnter' | 'onMouseLeave' | 'open'>
     prefix?: ReactNode
     radius?: Radius | Radius[]
     /** @beta */
