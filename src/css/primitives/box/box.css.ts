d336fba485119aa9191be2a9d6f192a61e91453a
Marius Lundgård <studio@mariuslundgard.com>
2025-09-05 10:37:33 +0200

    fix(box): transition muted background color

--- src/css/primitives/box/box.css.ts
+++ src/css/primitives/box/box.css.ts
@@ -27,6 +27,8 @@ export const root: string = _style(layers.primitives, {
 export const muted: string = _style(layers.primitives, {
   backgroundColor: vars.color.bg,
 
+  transition: 'background-color 100ms ease-in-out',
+
   selectors: {
     '&&': {
       vars: {
