5df49134f9d5c4f9639fbd91218e094dd39dd0da
Rita <anarita151@gmail.com>
2025-09-10 15:12:36 +0200

    docs: add comments

--- src/core/hooks/useGlobalKeyDown.ts
+++ src/core/hooks/useGlobalKeyDown.ts
@@ -2,6 +2,10 @@ import {useEffect} from 'react'
 import {useEffectEvent} from 'use-effect-event'
 
 /**
+ * Adds global keydown event listener to the window.
+ *
+ * @param onKeyDown - The function to call when a keydown event is triggered.
+ * @param options - The options to pass to the addEventListener function (example, capture: true)
  * @beta
  */
 export function useGlobalKeyDown(
