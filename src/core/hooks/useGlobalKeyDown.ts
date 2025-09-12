7fd41e0cb477aea6e198082c1cb4513b714757c9
RitaDias <anarita151@gmail.com>
2025-09-12 08:44:38 +0200

    feat: add options prop to useGlobalKeyDown (#2061)
    * feat: add options prop to useGlobalKeyDown
    
    * docs: add comments

--- src/core/hooks/useGlobalKeyDown.ts
+++ src/core/hooks/useGlobalKeyDown.ts
@@ -2,16 +2,23 @@ import {useEffect} from 'react'
 import {useEffectEvent} from 'use-effect-event'
 
 /**
+ * Adds global keydown event listener to the window.
+ *
+ * @param onKeyDown - The function to call when a keydown event is triggered.
+ * @param options - The options to pass to the addEventListener function (example, capture: true)
  * @beta
  */
-export function useGlobalKeyDown(onKeyDown: (event: KeyboardEvent) => void): void {
+export function useGlobalKeyDown(
+  onKeyDown: (event: KeyboardEvent) => void,
+  options?: AddEventListenerOptions,
+): void {
   const handleKeyDown = useEffectEvent((event: KeyboardEvent) => onKeyDown(event))
 
   useEffect(() => {
     const handler = (event: KeyboardEvent) => handleKeyDown(event)
 
-    window.addEventListener('keydown', handler)
+    window.addEventListener('keydown', handler, options)
 
-    return () => window.removeEventListener('keydown', handler)
-  }, [])
+    return () => window.removeEventListener('keydown', handler, options)
+  }, [options])
 }
