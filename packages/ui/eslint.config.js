234218dbf27917fd0eeaaf4cd6d26c81340aaf8a
Marius Lundgård <studio@mariuslundgard.com>
2026-02-13 17:59:56 +0100

    tmp - chore: fix eslint issues

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -33,7 +33,9 @@ export default ts.config(
           ...globals.es2018,
         },
         sourceType: 'module',
-        tsconfigRootDir: import.meta.dirname,
+        parserOptions: {
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
       linterOptions: {reportUnusedDisableDirectives: 'error'},
       rules: {
