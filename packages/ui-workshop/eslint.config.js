0221bac804d8516bf7bd516c8d1499b553aefd6c
Marius Lundgård <studio@mariuslundgard.com>
2026-02-24 11:47:52 +0100

    chore(ui-workshop): share vanilla-extract config

--- packages/ui-workshop/eslint.config.js
+++ packages/ui-workshop/eslint.config.js
@@ -9,18 +9,123 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import ts from 'typescript-eslint'
 
-export default ts.config(
-  [
-    {
-      ignores: ['.workshop', 'dist', '**/__fixtures__/**'],
+export default defineConfig(import.meta.dirname, [
+  {
+    ignores: ['**/__fixtures__/**', '.workshop', 'dist', 'tmp'],
+  },
+
+  // boundaries
+  {
+    plugins: {boundaries},
+
+    settings: {
+      'boundaries/ignore': ['src/**/*.test.ts', 'src/**/*.test.tsx', 'src/**/__workshop__/**/*'],
+
+      'boundaries/elements': [
+        // exports
+        {
+          type: '@sanity/ui-workshop',
+          pattern: ['exports/index.ts'],
+          mode: 'file',
+        },
+        {
+          type: '@sanity/ui/plugin-a11y',
+          pattern: ['exports/plugin-a11y.ts'],
+          mode: 'file',
+        },
+        {
+          type: '@sanity/ui/plugin-perf',
+          pattern: ['exports/plugin-perf.ts'],
+          mode: 'file',
+        },
+        {
+          type: '@sanity/ui/runtime',
+          pattern: ['exports/runtime.ts'],
+          mode: 'file',
+        },
+
+        // modules
+        {
+          type: 'src/cli',
+          pattern: ['src/cli/**/*'],
+          mode: 'full',
+        },
+        {
+          type: 'src/core',
+          pattern: ['src/core/**/*'],
+          mode: 'full',
+        },
+        {
+          type: 'src/plugin-a11y',
+          pattern: ['src/plugin-a11y/**/*'],
+          mode: 'full',
+        },
+        {
+          type: 'src/plugin-perf',
+          pattern: ['src/plugin-perf/**/*'],
+          mode: 'full',
+        },
+        {
+          type: 'src/runtime',
+          pattern: ['src/runtime/**/*'],
+          mode: 'full',
+        },
+
+        // tests
+        {
+          type: 'test',
+          pattern: ['test/**/*', 'src/**/*.test.*'],
+          mode: 'full',
+        },
+
+        // configs
+        {
+          type: 'configs',
+          pattern: [
+            'vanilla-extract/**/*',
+            'package.config.ts',
+            'workshop.config.ts',
+            'workshop.runtime.ts',
+          ],
+          mode: 'file',
+        },
+      ],
     },
 
-    {
-      extends: [
-        js.configs.recommended,
-        ...ts.configs.recommended,
-        _import.flatConfigs.recommended,
-        _import.flatConfigs.typescript,
+    rules: {
+      ...boundaries.configs.recommended.rules,
+
+      'boundaries/element-types': [
+        'error',
+        {
+          default: 'disallow',
+          rules: [
+            {from: '@sanity/ui-workshop', allow: ['src/core']},
+            {from: '@sanity/ui/plugin-a11y', allow: ['src/plugin-a11y']},
+            {from: '@sanity/ui/plugin-perf', allow: ['src/plugin-perf']},
+            {from: '@sanity/ui/runtime', allow: ['src/runtime']},
+
+            {
+              from: 'src/core',
+              allow: ['src/core'],
+            },
+            {from: 'src/plugin-a11y', allow: ['@sanity/ui-workshop', 'src/plugin-a11y']},
+            {from: 'src/plugin-perf', allow: ['@sanity/ui-workshop', 'src/plugin-perf']},
+            {from: 'src/runtime', allow: ['@sanity/ui-workshop', 'src/runtime']},
+
+            {from: 'configs', allow: ['configs']},
+
+            {
+              from: 'test',
+              allow: [
+                '@sanity/ui-workshop',
+                '@sanity/ui/plugin-a11y',
+                '@sanity/ui/plugin-perf',
+                '@sanity/ui/runtime',
+              ],
+            },
+          ],
+        },
       ],
       files: ['**/*.{ts,tsx}'],
       languageOptions: {
--- packages/ui-workshop/workshop.runtime.ts
+++ packages/ui-workshop/workshop.runtime.ts
@@ -1,10 +1,14 @@
+import path from 'node:path'
+import {fileURLToPath} from 'node:url'
+
 import {defineRuntime} from '@sanity/ui-workshop/runtime'
 import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin'
-import path from 'path'
-import {fileURLToPath} from 'url'
 import {mergeConfig} from 'vite'
 
+import {vanillaExtractIdentifiers} from './vanilla-extract/identifiers'
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 const DIST_PATH = path.resolve(__dirname, 'dist')
 const EXPORTS_PATH = path.resolve(__dirname, 'exports')
 
@@ -16,24 +20,7 @@ export default defineRuntime({
     const isProd = viteConfig.mode === 'production'
 
     return mergeConfig(viteConfig, {
-      plugins: [
-        vanillaExtractPlugin({
-          identifiers: (options) => {
-            const {debugId, hash, filePath} = options
-
-            if (isProd) {
-              return `ui-workshop-${hash}`
-            }
-
-            const basename = path.basename(filePath, '.css.ts')
-            const name = dashCase(
-              [basename, debugId && sanitize(debugId)].filter(Boolean).join('-'),
-            )
-
-            return `${name}-${hash}`
-          },
-        }),
-      ],
+      plugins: [vanillaExtractPlugin({identifiers: vanillaExtractIdentifiers})],
 
       optimizeDeps: {
         exclude: ['@sanity/ui-workshop'],
@@ -53,15 +40,3 @@ export default defineRuntime({
     })
   },
 })
-
-function dashCase(str: string): string {
-  return str
-    .replace(/([A-Z])/g, '-$1')
-    .toLowerCase()
-    .replace(/^-/, '')
-}
-
-function sanitize(str: string): string {
-  // remove all non-alphanumeric characters except for dashes
-  return str.replace(/[^a-zA-Z0-9-]/g, '')
-}
