9f626b405d613d8f4ded67778a9217f435122eea
Marius Lundgård <studio@mariuslundgard.com>
2026-02-24 11:50:11 +0100

    refactor(ui-workshop): simplify cli module

--- packages/ui-workshop/package.config.ts
+++ packages/ui-workshop/package.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 
   bundles: [
     {
-      source: './src/cli/index.ts',
+      source: './src/cli.ts',
       import: './dist/cli.js',
       runtime: 'node',
     },
