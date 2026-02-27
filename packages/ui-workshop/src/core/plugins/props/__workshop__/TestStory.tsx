d5a1a4ee72f4723632db946fe8325e58a9e6f0c6
Marius Lundgård <studio@mariuslundgard.com>
2026-02-27 15:01:30 +0100

    test(ui-workshop): update workshop stories

---
+++ packages/ui-workshop/src/core/plugins/props/__workshop__/TestStory.tsx
@@ -0,0 +1,29 @@
+import {Flex, Stack, Text} from '@sanity/ui'
+import {useBoolean, useNumber, useSelect, useString, useText} from '@sanity/ui-workshop'
+
+const options = {
+  None: '',
+  Small: 'sm',
+  Medium: 'md',
+  Large: 'lg',
+}
+
+export default function TestStory() {
+  const boolean = useBoolean('Boolean')
+  const number = useNumber('Number')
+  const option = useSelect('Select', options)
+  const string = useString('String', 'String')
+  const text = useText('Text', 'Text')
+
+  return (
+    <Flex align="center" height="fill" justify="center">
+      <Stack gap={4} maxWidth={1} padding={4} width="fill">
+        <Text muted>Boolean: {boolean ? 'true' : 'false'}</Text>
+        <Text muted>Number: {number}</Text>
+        <Text muted>Select: {option ?? <>&ndash;</>}</Text>
+        <Text muted>String: {string}</Text>
+        <Text muted>Text: {text}</Text>
+      </Stack>
+    </Flex>
+  )
+}
