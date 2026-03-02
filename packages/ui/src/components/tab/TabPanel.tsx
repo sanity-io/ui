8867834f74b9ffcc3e334478fce6276e8a136f9b
Marius Lundgård <studio@mariuslundgard.com>
2026-03-02 10:09:14 +0100

    docs(ui): improve inline comments

--- packages/ui/src/components/tab/TabPanel.tsx
+++ packages/ui/src/components/tab/TabPanel.tsx
@@ -9,7 +9,7 @@ export const DEFAULT_TAB_PANEL_ELEMENT = 'div'
 /** @public */
 export type TabPanelOwnProps = BoxOwnProps & {
   /**
-   * The `id` of the correlating `Tab` component.
+   * The `id` of the corresponding `Tab` component.
    */
   'aria-labelledby': string
   'id': string
@@ -37,7 +37,6 @@ export function TabPanel<E extends TabPanelElementType = typeof DEFAULT_TAB_PANE
   } = props as TabPanelProps<typeof DEFAULT_TAB_PANEL_ELEMENT>
 
   const ref = useRef<HTMLDivElement | null>(null)
-  // const focusedRef = useRef(false)
 
   useImperativeHandle<HTMLDivElement | null, HTMLDivElement | null>(forwardedRef, () => ref.current)
 
@@ -71,7 +70,6 @@ export function TabPanel<E extends TabPanelElementType = typeof DEFAULT_TAB_PANE
       className={tab_panel({className})}
       role="tabpanel"
       tabIndex={hasFocusableChildren ? undefined : 0}
-      // tabIndex={tabIndex === undefined ? 0 : tabIndex}
     >
       {children}
     </Box>
