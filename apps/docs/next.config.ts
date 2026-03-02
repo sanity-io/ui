a299aeb88f765586b989ec9b684090fc4e1401b8
Marius Lundgård <studio@mariuslundgard.com>
2026-03-02 14:27:13 +0100

    chore(docs): convert to typescript

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type {NextConfig} from 'next'
+
+const nextConfig: NextConfig = {
   basePath: '/ui',
   compiler: {
     styledComponents: true,
@@ -37,4 +38,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
