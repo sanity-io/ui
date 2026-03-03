512ab69b34464ac0cd2688385daa093f3a4baea4
Marius Lundgård <studio@mariuslundgard.com>
2026-03-03 12:54:31 +0100

    fix(docs): update favicons

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -16,11 +16,92 @@ import {getContext} from './context'
 export const metadata: Metadata = {
   title: 'Sanity UI',
   description: DEFAULT_META_DESCRIPTION,
-  icons: {
-    icon: '/sanity-favicon-48.png',
-    shortcut: '/sanity-favicon-32.png',
-    apple: '/sanity-favicon-57.png',
-  },
+  icons: [
+    {
+      type: 'image/png',
+      url: '/android-icon-192x192.png',
+      sizes: '192x192',
+      rel: 'icon',
+    },
+    {
+      type: 'image/png',
+      url: '/favicon-96x96.png',
+      sizes: '96x96',
+      rel: 'icon',
+    },
+    {
+      type: 'image/png',
+      url: '/favicon-32x32.png',
+      sizes: '32x32',
+      rel: 'icon',
+    },
+    {
+      type: 'image/png',
+      url: '/favicon-16x16.png',
+      sizes: '16x16',
+      rel: 'icon',
+    },
+    {
+      // type: 'image/svg+xml',
+      url: '/safari-pinned-tab.svg',
+      color: 'black',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-57x57.png',
+      sizes: '57x57',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-60x60.png',
+      sizes: '60x60',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-72x72.png',
+      sizes: '72x72',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-76x76.png',
+      sizes: '76x76',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-114x114.png',
+      sizes: '114x114',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-120x120.png',
+      sizes: '120x120',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-144x144.png',
+      sizes: '144x144',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-152x152.png',
+      sizes: '152x152',
+      rel: 'apple-touch-icon',
+    },
+    {
+      type: 'image/png',
+      url: '/apple-icon-180x180.png',
+      sizes: '180x180',
+      rel: 'apple-touch-icon',
+    },
+  ],
   openGraph: {
     type: 'website',
     title: 'Sanity UI',
