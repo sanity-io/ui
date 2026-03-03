99f44f0e789dc43d7916b70a88e353dff050a67f
Marius Lundgård <studio@mariuslundgard.com>
2026-03-03 11:59:07 +0100

    fix(docs): draft mode endpoints

---
+++ apps/docs/src/app/api/draft-mode/enable/route.ts
@@ -0,0 +1,17 @@
+import {defineEnableDraftMode} from 'next-sanity/draft-mode'
+import {cookies} from 'next/headers'
+
+import {getContext} from '@/app/context'
+import {AppEnv} from '@/app/types'
+
+export const GET = async (request: Request) => {
+  const url = new URL(request.url)
+  const env = (url.searchParams.get('env') ?? 'production') as AppEnv
+
+  // set `env` cookie
+  ;(await cookies()).set('env', env)
+
+  const {client} = await getContext()
+
+  return defineEnableDraftMode({client}).GET(request)
+}
