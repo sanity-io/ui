830c0ddd11e6cf753e529f74b89962df2f5b6e1b
Marius Lundgård <studio@mariuslundgard.com>
2026-03-04 12:58:09 +0100

    fix(docs): nav tree query

--- apps/docs/src/lib/data/_global/query.ts
+++ apps/docs/src/lib/data/_global/query.ts
@@ -47,10 +47,10 @@ export const SETTINGS_QUERY = groq`*[_id == "settings"]{_id,banner}[0]`
 
 export const GLOBAL_QUERY = groq`{
   "nav": ${MAIN_NAV_QUERY},
-  "navTrees": *[_type == "nav" && defined(id)]{
+  "navTrees": *[_type == "nav" && defined(id) && id != "main"]{
     _id,
     id,
     title
-  },
+  }|order(id asc),
   "settings": ${SETTINGS_QUERY}
 }`
