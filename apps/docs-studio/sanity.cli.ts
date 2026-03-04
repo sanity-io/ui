a0b080eb4949d4b9d7b994861b43df3cc1ef6d72
Marius Lundgård <studio@mariuslundgard.com>
2026-03-04 12:19:12 +0100

    chore(docs-studio): configure sanity deployment

--- apps/docs-studio/sanity.cli.ts
+++ apps/docs-studio/sanity.cli.ts
@@ -1,6 +1,14 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
+  api: {
+    projectId: process.env['SANITY_STUDIO_PROJECT_ID'],
+  },
+
+  deployment: {
+    appId: 'dcsu4kassupf18r9gb4jk340',
+  },
+
   vite: (viteConfig) => ({
     ...viteConfig,
     resolve: {
