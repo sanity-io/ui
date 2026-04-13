e52352e23b6efb5c6f805264ed623afe3eb5ed8a
Marius Lundgård <studio@mariuslundgard.com>
2026-04-13 13:26:48 +0200

    build(ui): add `prebuild` step

---
+++ packages/ui/scripts/prebuild.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import {fileURLToPath} from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+async function prebuild() {
+  const packagePath = path.resolve(__dirname, '..')
+
+  const _rel = (p: string) => path.relative(packagePath, path.resolve(packagePath, p))
+
+  const exportsPath = path.resolve(packagePath, 'exports')
+  const distPath = path.resolve(packagePath, 'dist')
+
+  // remove `dist` directory
+  await fs.rm(distPath, {force: true, recursive: true})
+  console.log(`- removed ${_rel(distPath)}`)
+
+  // copy `exports/css/index.css` to `dist/css/index.css`
+  await fs.cp(path.resolve(exportsPath, 'css/index.css'), path.resolve(distPath, 'css/index.css'))
+  console.log(
+    `- copied ${_rel(path.resolve(exportsPath, 'css/index.css'))} to ${_rel(path.resolve(distPath, 'css/index.css'))}`,
+  )
+}
+
+prebuild().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
