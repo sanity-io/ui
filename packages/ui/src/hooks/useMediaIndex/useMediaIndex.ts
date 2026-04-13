3be5575efa18c34cde6bc3380eb5a0f6d9ed8edf
Marius Lundgård <studio@mariuslundgard.com>
2026-04-13 13:20:12 +0200

    refactor(ui): derive media queries directly from `BREAKPOINTS`

--- packages/ui/src/hooks/useMediaIndex/useMediaIndex.ts
+++ packages/ui/src/hooks/useMediaIndex/useMediaIndex.ts
@@ -1,8 +1,6 @@
 import {type Breakpoint, BREAKPOINTS} from '@sanity/ui-css'
 import {useMemo, useSyncExternalStore} from 'react'
 
-const media = Object.values(BREAKPOINTS)
-
 /**
  * @internal
  */
@@ -16,22 +14,27 @@ type MediaQueryMaxWidth = `(max-width: ${number}px)`
 type MediaQueryMinMaxWidth = `${MediaQueryMinWidth} and ${MediaQueryMaxWidth}`
 type MediaQuery = `screen and ${MediaQueryMinWidth | MediaQueryMaxWidth | MediaQueryMinMaxWidth}`
 
-function _getMediaQuery(media: number[], index: number): MediaQuery {
+function _getMediaQuery(index: number): MediaQuery {
   if (index === 0) {
-    return `screen and (max-width: ${media[index + 1] - 1}px)`
+    return `screen and (max-width: ${BREAKPOINTS[index + 1] - 1}px)`
   }
 
-  if (index === media.length - 1) {
-    return `screen and (min-width: ${media[index]}px)`
+  if (index === BREAKPOINTS.length - 1) {
+    return `screen and (min-width: ${BREAKPOINTS[index]}px)`
   }
 
-  return `screen and (min-width: ${media[index]}px) and (max-width: ${media[index + 1] - 1}px)`
+  return `screen and (min-width: ${BREAKPOINTS[index]}px) and (max-width: ${BREAKPOINTS[index + 1] - 1}px)`
 }
 
 function _createMediaStore(): _MediaStore {
-  const mediaLen = media.length
+  const mediaLen = BREAKPOINTS.length
 
-  let sizes: {mq: MediaQueryList; index: number}[] | undefined = undefined
+  let sizes:
+    | {
+        index: number
+        mq: MediaQueryList
+      }[]
+    | undefined = undefined
 
   // The _createMediaStore function is called in both server and client environments.
   // However since subscribe and getSnapshot are only called on the client we lazy init what we need for them
@@ -45,9 +48,10 @@ function _createMediaStore(): _MediaStore {
       sizes = []
 
       for (let index = mediaLen; index > -1; index -= 1) {
-        const mediaQuery = _getMediaQuery(media, index)
-
-        sizes.push({index, mq: window.matchMedia(mediaQuery)})
+        sizes.push({
+          index,
+          mq: window.matchMedia(_getMediaQuery(index)),
+        })
       }
     }
 
