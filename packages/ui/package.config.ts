3432cebe14322fb3599ea839fb507fc7936fd5a0
Marius Lundgård <studio@mariuslundgard.com>
2026-04-15 19:41:53 +0200

    build(ui): update package config

--- packages/ui/package.config.ts
+++ packages/ui/package.config.ts
@@ -2,10 +2,11 @@ import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
   babel: {reactCompiler: true},
-  // dts: 'rolldown',
+  dts: 'rolldown',
   extract: {
     rules: {
       'ae-internal-missing-underscore': 'off',
+      'ae-incompatible-release-tags': 'warn',
     },
   },
   reactCompilerOptions: {target: '19'},
