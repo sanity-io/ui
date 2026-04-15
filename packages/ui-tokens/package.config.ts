8901729dd4d575e42b942a1ff81b111b8a4abccf
Marius Lundgård <studio@mariuslundgard.com>
2026-04-15 19:39:54 +0200

    build(ui-tokens): update package config

--- packages/ui-tokens/package.config.ts
+++ packages/ui-tokens/package.config.ts
@@ -1,10 +1,11 @@
 import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
-  tsconfig: 'tsconfig.dist.json',
+  dts: 'rolldown',
   extract: {
     rules: {
       'ae-internal-missing-underscore': 'off',
     },
   },
+  tsconfig: 'tsconfig.dist.json',
 })
