2484a190181c6c2e74b06e737fdde73be8871129
Marius Lundgård <studio@mariuslundgard.com>
2026-04-16 12:43:08 +0200

    fix(ui-css): inherit badge colors inside button and selectable

--- packages/ui-css/src/primitives/badge/badge.css.ts
+++ packages/ui-css/src/primitives/badge/badge.css.ts
@@ -1,7 +1,8 @@
 import {_layers} from '../../layers.css'
 import {_style} from '../../lib/css/_style.css'
-// import {root as elementToneRoot} from '../../props/elementTone/elementTone.css'
 import {vars} from '../../vars'
+import {root as buttonRoot} from '../button/button.css'
+import {root as selectableRoot} from '../selectable/selectable.css'
 
 export const root: string = _style(
   _layers.primitive,
@@ -10,20 +11,26 @@ export const root: string = _style(
     color: vars.color.muted.fg,
     whiteSpace: 'nowrap',
 
-    // selectors: {
-    //   [`&:not(${elementToneRoot} &)`]: {
-    //     vars: {
-    //       [vars.color.muted.bg]: vars.color.tinted.bg[2],
-    //       [vars.color.muted.border]: vars.color.tinted.border[2],
-    //       [vars.color.muted.fg]: vars.color.tinted.fg[3],
-    //     },
-    //   },
-    // },
-
     vars: {
-      // [vars.color.bg]: vars.color.muted.bg,
+      [vars.color.bg]: vars.color.muted.bg,
       [vars.color.border]: vars.color.muted.border,
       [vars.color.fg]: vars.color.muted.fg,
+      [vars.color.muted.bg]: vars.color.tinted.bg[2],
+      [vars.color.muted.border]: vars.color.tinted.border[2],
+      [vars.color.muted.fg]: vars.color.tinted.fg[3],
+    },
+
+    selectors: {
+      [`:where(${buttonRoot}, ${selectableRoot}) &`]: {
+        vars: {
+          [vars.color.bg]: 'inherit',
+          [vars.color.border]: 'inherit',
+          [vars.color.fg]: 'inherit',
+          [vars.color.muted.bg]: 'inherit',
+          [vars.color.muted.border]: 'inherit',
+          [vars.color.muted.fg]: 'inherit',
+        },
+      },
     },
   },
   '',
