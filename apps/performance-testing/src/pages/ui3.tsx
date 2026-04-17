39c9740485bda6d3d86ae18d2d89ab7371a08c08
laurenashpole <lauren.ashpole@sanity.io>
2026-04-17 16:11:32 -0400

    chore: use shared eslint config in performance-testing app

--- apps/performance-testing/src/pages/ui3.tsx
+++ apps/performance-testing/src/pages/ui3.tsx
@@ -1,6 +1,6 @@
-import { Box, ThemeProvider } from '@sanity/ui'
-import { buildTheme } from '@sanity/ui/theme'
-import { Profiler } from 'react'
+import {Box, ThemeProvider} from '@sanity/ui'
+import {buildTheme} from '@sanity/ui/theme'
+import {Profiler} from 'react'
 
 const count = 5000
 const iterator = [...Array(count).keys()]
@@ -11,12 +11,12 @@ function handleOnRender(
   actualDuration: number,
   baseDuration: number,
   startTime: number,
-  commitTime: number
+  commitTime: number,
 ) {
-  const value = { id, phase, actualDuration, baseDuration, startTime, commitTime }
+  const value = {id, phase, actualDuration, baseDuration, startTime, commitTime}
   const perComponent = actualDuration / count
   console.log(JSON.stringify(value, null, 2))
-  console.log({ perComponent })
+  console.log({perComponent})
   navigator.clipboard.writeText(String(perComponent))
 }
 
@@ -27,7 +27,7 @@ export default function Ui3() {
     <ThemeProvider theme={theme}>
       <h1>UI v3</h1>
       <h2>{count} Boxes</h2>
-      <Profiler id='Boxes' onRender={handleOnRender}>
+      <Profiler id="Boxes" onRender={handleOnRender}>
         {iterator.map((i) => (
           <Box key={i} padding={3} marginBottom={2}>
             <span>Box content</span>
--- apps/performance-testing/src/pages/ui4.tsx
+++ apps/performance-testing/src/pages/ui4.tsx
@@ -1,7 +1,7 @@
 import 'ui4/css/index.css'
 
-import { Profiler } from 'react'
-import { Box, Root } from 'ui4'
+import {Profiler} from 'react'
+import {Box, Root} from 'ui4'
 
 const count = 5000
 const iterator = [...Array(count).keys()]
@@ -12,21 +12,21 @@ function handleOnRender(
   actualDuration: number,
   baseDuration: number,
   startTime: number,
-  commitTime: number
+  commitTime: number,
 ) {
-  const value = { id, phase, actualDuration, baseDuration, startTime, commitTime }
+  const value = {id, phase, actualDuration, baseDuration, startTime, commitTime}
   const perComponent = actualDuration / count
   console.log(JSON.stringify(value, null, 2))
-  console.log({ perComponent })
+  console.log({perComponent})
   navigator.clipboard.writeText(String(perComponent))
 }
 
 export default function Ui4() {
   return (
-    <Root lang='en'>
+    <Root lang="en">
       <h1>UI v4</h1>
       <h2>{count} Boxes</h2>
-      <Profiler id='Boxes' onRender={handleOnRender}>
+      <Profiler id="Boxes" onRender={handleOnRender}>
         {iterator.map((i) => (
           <Box key={i} padding={3} marginBottom={2}>
             <span>Box content</span>
