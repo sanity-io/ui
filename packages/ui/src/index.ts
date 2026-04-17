c18ba7b68f1b55721286536b303f937106395e9d
laurenashpole <lauren.ashpole@sanity.io>
2026-04-17 16:31:20 -0400

    fix: update component index

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,4 @@
-export * from './components/Box'
+export * from './components/box/Box'
+export * from './components/flex/Flex'
+export * from './components/grid/Grid'
+export * from './components/text/Text'
