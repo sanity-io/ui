92141bc8a05a79b52e1c3e4b0015a595537e3867
Marius Lundgård <studio@mariuslundgard.com>
2026-04-17 19:27:45 +0200

    test(ui): improve code workshop

--- packages/ui/src/primitives/code/__workshop__/Props.tsx
+++ packages/ui/src/primitives/code/__workshop__/Props.tsx
@@ -1,7 +1,7 @@
 import {Box, Code, FONT_CODE_SIZE} from '@sanity/ui'
 import {useSelect, useText} from '@sanity/ui-workshop'
 
-import {WORKSHOP_CODE_LANGUAGE_OPTIONS} from '$workshop'
+import {CardWrapper, WORKSHOP_CODE_LANGUAGE_OPTIONS} from '$workshop'
 
 export default function PropsStory(): React.JSX.Element {
   const code = useText('Code', `console.log('Hello, world')`)
@@ -9,10 +9,12 @@ export default function PropsStory(): React.JSX.Element {
   const size = useSelect('Size', [undefined, ...FONT_CODE_SIZE], 1)
 
   return (
-    <Box padding={[4, 5, 6]}>
-      <Code language={language} size={size}>
-        {code}
-      </Code>
-    </Box>
+    <CardWrapper alignItems="center" display="flex" justifyContent="center">
+      <Box muted overflow="auto" padding={3} radius={3}>
+        <Code language={language} size={size}>
+          {code}
+        </Code>
+      </Box>
+    </CardWrapper>
   )
 }
