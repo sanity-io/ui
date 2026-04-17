f62c49bf44bdbb4f0a6a95c15e71f44462789f64
laurenashpole <lauren.ashpole@sanity.io>
2026-04-17 11:45:57 -0400

    chore: start shared eslint config

---
+++ packages/@repo/eslint-config/index.mjs
@@ -0,0 +1,53 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import * as importPlugin from 'eslint-plugin-import'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+/** @type import('eslint').Linter.Config */
+export default defineConfig([
+  js.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parserOptions: {},
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'import': importPlugin,
+    },
+    rules: {
+      'no-console': 'error',
+      'no-shadow': 'error',
+      'no-warning-comments': [
+        'warn',
+        {
+          location: 'start',
+          terms: ['todo', 'fixme'],
+        },
+      ],
+      'quote-props': ['warn', 'consistent-as-needed'],
+      'simple-import-sort/exports': 'warn',
+      'simple-import-sort/imports': 'warn',
+      'strict': ['warn', 'global'],
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "error",
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/member-delimiter-style": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+    },
+  },
+])
