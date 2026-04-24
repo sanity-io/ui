a7a683029ca7fb63b8fdcdea00afdfb1c0bab71c
laurenashpole <lauren.ashpole@sanity.io>
2026-04-24 18:35:37 -0400

    refactor: combined v3 and v4 codemods

--- packages/ui-codemod/src/transforms/v4-to-v5/grid-props.test.ts
+++ packages/ui-codemod/src/transforms/v4-to-v5/grid-props.test.ts
@@ -57,3 +57,41 @@ defineInlineTest(
   `,
   'updates grid props mapped values',
 )
+
+defineInlineTest(
+  transform,
+  {},
+  `
+  <Grid
+    autoCols="auto"
+    autoFlow="row"
+    autoRows="auto"
+  />
+  `,
+  `
+  <Grid
+    gridAutoColumns="auto"
+    gridAutoFlow="row"
+    gridAutoRows="auto"
+  />
+  `,
+  'renames v3 grid props',
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `
+  <Grid
+    columns={1}
+    rows={2}
+  />
+  `,
+  `
+  <Grid
+    gridTemplateColumns="repeat(1, 1fr)"
+    gridTemplateRows="repeat(2, 1fr)"
+  />
+  `,
+  'updates v3 grid props mapped values',
+)
--- packages/ui-codemod/src/transforms/v4-to-v5/grid-props.ts
+++ packages/ui-codemod/src/transforms/v4-to-v5/grid-props.ts
@@ -12,6 +12,18 @@ const MODS: AttributeMods = {
     type: 'style-only',
     style: 'alignItems',
   },
+  autoCols: {
+    type: 'rename-only',
+    name: 'gridAutoColumns',
+  },
+  autoFlow: {
+    type: 'rename-only',
+    name: 'gridAutoFlow',
+  },
+  autoRows: {
+    type: 'rename-only',
+    name: 'gridAutoRows',
+  },
   flexDirection: {
     type: 'style-only',
     style: 'flexDirection',
@@ -19,6 +31,25 @@ const MODS: AttributeMods = {
   flexWrap: {
     type: 'style-only',
     style: 'flexWrap',
+  },
+  columns: {
+    type: 'rename-mapped',
+    name: 'gridTemplateColumns',
+    mapping: {
+      0: '0px',
+      1: 'repeat(1, 1fr)',
+      2: 'repeat(2, 1fr)',
+      3: 'repeat(3, 1fr)',
+      4: 'repeat(4, 1fr)',
+      5: 'repeat(5, 1fr)',
+      6: 'repeat(6, 1fr)',
+      7: 'repeat(7, 1fr)',
+      8: 'repeat(8, 1fr)',
+      9: 'repeat(9, 1fr)',
+      10: 'repeat(10, 1fr)',
+      11: 'repeat(11, 1fr)',
+      12: 'repeat(12, 1fr)',
+    },
   },
   gridTemplateColumns: {
     type: 'mapped-only',
@@ -59,6 +90,25 @@ const MODS: AttributeMods = {
   justifyContent: {
     type: 'style-only',
     style: 'justifyContent',
+  },
+  rows: {
+    type: 'rename-mapped',
+    name: 'gridTemplateRows',
+    mapping: {
+      0: '0px',
+      1: 'repeat(1, 1fr)',
+      2: 'repeat(2, 1fr)',
+      3: 'repeat(3, 1fr)',
+      4: 'repeat(4, 1fr)',
+      5: 'repeat(5, 1fr)',
+      6: 'repeat(6, 1fr)',
+      7: 'repeat(7, 1fr)',
+      8: 'repeat(8, 1fr)',
+      9: 'repeat(9, 1fr)',
+      10: 'repeat(10, 1fr)',
+      11: 'repeat(11, 1fr)',
+      12: 'repeat(12, 1fr)',
+    },
   },
 }
 
