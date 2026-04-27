04c02579d4e7cdd590630402f04799d0b30ef0bc
laurenashpole <lauren.ashpole@sanity.io>
2026-04-27 11:42:18 -0400

    fix: add mods to typography codemods

--- packages/ui-codemod/src/transforms/latest/text.test.ts
+++ packages/ui-codemod/src/transforms/latest/text.test.ts
@@ -21,12 +21,40 @@ defineInlineTest(
   transform,
   {},
   `
-  <Text/>
+  <Text />
   `,
   `
   <Text trim={true} />
   `,
   'adds trim prop',
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `
+  <Text flex="auto" />
+  `,
+  `
+  <Text
+    style={{
+      flex: "1 1 auto"
+    }}
+    trim={true} />
+  `,
+  'moves flex prop to style',
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `
+  <Text accent />
+  `,
+  `
+  <Text tone="suggest" trim={true} />
+  `,
+  'renames accent prop and updates mapped values',
 )
 
 defineInlineTest(
@@ -39,4 +67,16 @@ defineInlineTest(
   <Text lineClamp={1} trim={true} />
   `,
   'renames textOverflow prop and updates mapped values',
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `
+  <Text maxWidth="fill" />
+  `,
+  `
+  <Text maxWidth="100%" trim={true} />
+  `,
+  'updates maxWidth prop mapped value',
 )
