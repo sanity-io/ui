ede04aa9126cc14f319c3198a02fdc9cc658545a
Marius Lundgård <studio@mariuslundgard.com>
2026-04-28 13:14:17 +0200

    feat(ui-tokens): add component color aliases

--- packages/ui-tokens/src/lib/color/_colorAlias.ts
+++ packages/ui-tokens/src/lib/color/_colorAlias.ts
@@ -1,7 +1,8 @@
 import type {
+  _ButtonState,
+  _CodeTokenKey,
   AvatarColor,
   CardTone,
-  CodeTokenKey,
   ColorVariant,
   ElementTone,
   Hue,
@@ -23,11 +24,17 @@ type ColorSemanticKey =
   | 'shadow.penumbra'
   | 'shadow.ambient'
 
-type ColorCodeTokenKey = `code.token.${CodeTokenKey}`
+type ColorCodeTokenKey = `code.token.${_CodeTokenKey}`
 
 type ColorElementKey = `${ColorVariant}.${'bg' | 'border' | 'fg'}.${0 | 1 | 2 | 3 | 4}`
 
 type ColorAvatarKey = `avatar.${AvatarColor}.${'bg' | 'fg'}`
+
+type BooleanState = 'unchecked' | 'checked'
+
+type InputState = 'enabled' | 'hovered' | 'disabled'
+
+type SelectableState = 'enabled' | 'hovered' | 'pressed' | 'selected' | 'disabled'
 
 type ColorKey =
   | `color.${ColorPaletteKey}`
@@ -39,6 +46,13 @@ type ColorKey =
   | `color._colorScheme.${ColorCodeTokenKey}`
   | `color._colorScheme.${CardTone}.${ColorSemanticKey}`
   | `color._colorScheme.${CardTone}.${ColorVariant}.${ElementTone}.${'bg' | 'border' | 'fg'}.${0 | 1 | 2 | 3 | 4}`
+  | `boolean.color.${'valid' | 'invalid'}.${BooleanState}.${InputState}.${'bg' | 'border' | 'fg' | 'muted.bg' | 'muted.border' | 'muted.fg'}`
+  | `boolean.color.${BooleanState}.${InputState}.${'bg' | 'border' | 'fg' | 'muted.bg' | 'muted.border' | 'muted.fg'}`
+  | `button.color.${'bg' | 'border' | 'fg' | 'muted.bg' | 'muted.border' | 'muted.fg'}`
+  | `button.color.${_ButtonState}.${'bg' | 'border' | 'fg' | 'muted.bg' | 'muted.border' | 'muted.fg'}`
+  | `input.color.${'valid' | 'invalid'}.${InputState}.${'bg' | 'border' | 'fg' | 'muted.bg' | 'muted.border' | 'muted.fg'}`
+  | `input.color.${InputState}.${'bg' | 'border' | 'fg' | 'muted.bg' | 'muted.border' | 'muted.fg'}`
+  | `selectable.color.${SelectableState}.${'bg' | 'border' | 'fg' | 'muted.bg' | 'muted.border' | 'muted.fg'}`
 
 type ColorAlias = `{${ColorKey}}`
 
