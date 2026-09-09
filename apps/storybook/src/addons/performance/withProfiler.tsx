9f47284c8d4d752e3c60de5f38b4927627ead84b
Cody Olsen <81981+stipsan@users.noreply.github.com>
2026-09-09 13:26:44 -0700

    Update withProfiler.tsx
    Co-authored-by: Lauren Ashpole <lauren@laurenashpole.com>

--- apps/storybook/src/addons/performance/withProfiler.tsx
+++ apps/storybook/src/addons/performance/withProfiler.tsx
@@ -17,8 +17,7 @@ const channel = addons.getChannel()
 
 const HIDDEN_BENCHMARK_STYLE: CSSProperties = {
   border: 0,
-  // oxlint-disable-next-line typescript/no-deprecated
-  clip: 'rect(0, 0, 0, 0)',
+clipPath: 'rect(0, 0, 0, 0)',
   height: 1,
   margin: -1,
   overflow: 'hidden',
