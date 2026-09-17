f01003c54ec1e011a7ce6d4f0aa920e60be01171
laurenashpole <lauren.ashpole@sanity.io>
2026-09-17 14:43:01 -0400

    fix: simplify menu and submenu, add submenu button

--- packages/ui/src/components/menu/Menu.tsx
+++ packages/ui/src/components/menu/Menu.tsx
@@ -1,31 +1,32 @@
+import {ChevronRightIcon} from '@sanity/icons/ChevronRight'
 import clsx from 'clsx'
-import {useId, type PointerEvent} from 'react'
+import {type ComponentPropsWithRef, type PointerEvent} from 'react'
 
-import {getProps} from '../../utils/getProps'
 import {suffixClassName} from '../../utils/suffixClassName'
+import {Icon} from '../icon/Icon'
 import {List} from '../list/List'
 import {Popover} from '../popover/Popover'
-import {type MenuProps, menuProps, menuSubmenuProps, type MenuSubmenuProps} from './menu.props'
+import {type MenuProps, type MenuSubmenuProps} from './menu.props'
 
 const menuClassName = suffixClassName('sui-Menu')
 const menuSubmenuClassName = suffixClassName('sui-MenuSubmenu')
 // const menuContentClassName = suffixClassName('sui-MenuContent')
 
-function MenuRoot(props: MenuProps) {
-  const {children, className, style, menu, ...rest} = getProps(props, menuProps)
-
+function MenuRoot({
+  menu,
+  children,
+  className,
+  style,
+  ...props
+}: MenuProps & Omit<ComponentPropsWithRef<'nav'>, keyof MenuProps>) {
   return (
     <Popover
       as="nav"
-      className={clsx(menuClassName, 'sui-radius3', className)}
-      style={style}
       data-ui="Menu"
-      content={
-        <List gap={1} className="sui-mx-1">
-          {menu}
-        </List>
-      }
-      {...rest}
+      content={<List gap={1}>{menu}</List>}
+      {...props}
+      className={clsx(menuClassName, className)}
+      style={{padding: 'var(--space-1)', ...style}}
     >
       {children}
     </Popover>
@@ -50,27 +51,38 @@ const MenuButtonItem: typeof List.ButtonItem = (props) => (
   />
 )
 
-function MenuSubmenu(props: MenuSubmenuProps) {
-  const {children, className, style, id: idProp, menu, ...rest} = getProps(props, menuSubmenuProps)
-  const reactId = useId()
-  const id = idProp || reactId
+const MenuSubmenuButtonItem: typeof List.ButtonItem = (props) => (
+  <List.ButtonItem
+    data-ui="MenuButtonItem"
+    density="compact"
+    className="sui-text-body1 sui-weight-medium"
+    end={<Icon icon={ChevronRightIcon} size={1} marginRight={-1} />}
+    {...props}
+  />
+)
 
+function MenuSubmenu({
+  menu,
+  children,
+  className,
+  style,
+  ...props
+}: MenuSubmenuProps & Omit<ComponentPropsWithRef<'ul'>, keyof MenuSubmenuProps>) {
   return (
     <Popover
       as={List}
-      className={clsx(menuSubmenuClassName, 'sui-radius3', className)}
-      style={style}
       data-ui="MenuSubmenu"
       content={menu}
-      id={id}
       placement="right-start"
       portal
-      {...rest}
+      {...props}
+      className={clsx(menuSubmenuClassName, className)}
+      style={{padding: 'var(--space-1)', ...style}}
       triggerProps={{
-        onPointerEnter: (event: PointerEvent<HTMLElement>) => {
-          const popover = document.getElementById(id)
+        onPointerEnter: (event: PointerEvent<HTMLButtonElement>) => {
+          const popover = event.currentTarget.popoverTargetElement
 
-          if (!popover || popover.matches(':popover-open')) {
+          if (!(popover instanceof HTMLElement) || popover.matches(':popover-open')) {
             return
           }
 
@@ -88,4 +100,5 @@ export const Menu = Object.assign(MenuRoot, {
   Item: MenuItem,
   ButtonItem: MenuButtonItem,
   Submenu: MenuSubmenu,
+  SubmenuButtonItem: MenuSubmenuButtonItem,
 })
